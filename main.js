38eff66b542c504e3408e5a507d05c1381b61eaf
Tuomas <tniinisto@gmail.com>
2013-05-04 15:26:17 +0300

    Players php & widget

--- main.js
+++ main.js
@@ -21,3 +21,23 @@ function showUser(str) {
 	xmlhttp.open("GET", "database.php?q=" + str, true);
 	xmlhttp.send();
 }
+
+function getPlayers() {
+	
+	if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
+		xmlhttp = new XMLHttpRequest();
+	}
+	else {// code for IE6, IE5
+		xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+	}
+
+	xmlhttp.onreadystatechange = function () {
+		if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+			document.getElementById("players_short").innerHTML = xmlhttp.responseText;
+		}
+	}
+
+	//alert("GET gets called.");
+	xmlhttp.open("GET", "players_short.php", true);
+	xmlhttp.send();
+}
---
+++ players_short.php
@@ -0,0 +1,32 @@
+ <?php
+
+	$q=$_GET["q"];
+
+	$con = mysql_connect('eu-cdbr-azure-north-a.cloudapp.net', 'bd3d44ed2e1c4a', '8ffac735');
+	if (!$con)
+	  {
+	  die('Could not connect: ' . mysql_error());
+	  }
+
+	mysql_select_db("areyouin", $con);
+
+	$sql="SELECT name, photourl FROM players";
+	
+	//echo $sql;
+	//echo "<br />";
+	//echo "<br />";
+
+	$result = mysql_query($sql);
+  	
+	echo <ul id="players_short">
+	while($row = mysql_fetch_array($result))
+	{
+		echo "<li> <img src=\"images/" . $row['photourl'] . "\" ". .$row['name'] . "></li>";
+		//echo "<li>" .$row['name'] . "</li>";
+	}
+	echo "</ul>>";
+
+	mysql_close($con);
+
+  ?>
+  
