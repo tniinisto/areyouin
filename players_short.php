7f95151e880e0d141c4003413ad03b9b281969f1
Tuomas <tniinisto@gmail.com>
2013-05-05 07:44:03 +0300

    asd

--- players_short.php
+++ players_short.php
@@ -23,8 +23,8 @@
 	echo "<ul id=\"players_short\">";
 	while($row = mysql_fetch_array($result))
 	{
-		echo "<li> <img width=\"50\" height=\"50\" src=\"images/" . $row['photourl'] . "\"&nbsp&nbsp&nbsp&nbsp&nbsp" . $row['name'] . "></li>";
-		//echo "<li>" .$row['name'] . "</li>";
+		echo "<li> <img width=\"50\" height=\"50\" src=\"images/" . $row['photourl'] . "\"" . $row['name'] . "></li>";
+		echo "<li>" .$row['name'] . "</li>";
 	}
 	echo "</ul>";
 
