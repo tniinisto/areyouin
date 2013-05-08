73eb5a8751f5bd8222888110f262b23436782a55
Tuomas <tniinisto@gmail.com>
2013-05-08 09:51:19 +0300

    event php

--- event_list.php
+++ event_list.php
@@ -12,37 +12,36 @@
 
 	//$sql="SELECT * FROM players";
 	$sql = "select l.name as location, e.startTime, e.endTime, p.name, v.areyouin from events e, eventtype t, location l, players p,  eventplayer v where t.eventTypeID = e.EventType_eventTypeID and l.locationID = e.Location_locationID and p.playerID = v.Players_playerID and v.Events_eventID = e.eventID";
-	
-	echo $sql;
-	//echo "<br />";
-	//echo "<br />";
-
-	$result = mysql_query($sql);
-  
+	 
 	//Table for event basic information
+	$result1 = mysql_query($sql);
+	
 	echo "<table border='0' id='atable'> 
 	<tr>
 	<th>Location</th>
 	<th>Start time</th>
 	<th>End time</th>
 	</tr>";
-	while($row = mysql_fetch_array($result))
+	while($row = mysql_fetch_array($result1))
 	{
 		echo "<tr>";
 		echo "<td>" . $row['location'] . "</td>";
 		echo "<td>" . $row['startTime'] . "</td>";
 		echo "<td>" . $row['endTime'] . "</td>";
 		echo "</tr>";
+		break;
 	}
 	echo "</table>";
 
 	//Table for event participants
+	$result2 = mysql_query($sql);
+	
 	echo "<table border='0' id='atable'> 
 		<tr>
 		<th>Player</th>
 		<th>A'youIN</th>
 		</tr>";	
-	while($row = mysql_fetch_array($result))
+	while($row = mysql_fetch_array($result2))
 	{
 		echo "<tr>";
 		echo "<td>" . $row['name'] . "</td>";
