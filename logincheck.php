75e4f2d1447880a1bfb7414bdedf6572d8d73794
Tuomas <tniinisto@gmail.com>
2013-05-21 12:14:00 +0300

    logincheck.php added

---
+++ logincheck.php
@@ -0,0 +1,51 @@
+<?php
+
+/*$host="localhost"; // Host name
+$username=""; // Mysql username
+$password=""; // Mysql password
+$db_name="test"; // Database name
+$tbl_name="members"; // Table name*/
+
+	$q=$_GET["q"];
+
+	$con = mysql_connect('eu-cdbr-azure-north-a.cloudapp.net', 'bd3d44ed2e1c4a', '8ffac735');
+	if (!$con)
+	  {
+	  die('Could not connect: ' . mysql_error());
+	  }
+
+	mysql_select_db("areyouin", $con)or die("cannot select DB");
+
+// Connect to server and select databse.
+/*mysql_connect("$host", "$username", "$password")or die("cannot connect");
+mysql_select_db("$db_name")or die("cannot select DB");
+*/
+
+	// username and password sent from form
+	$myusername=$_POST['ayiloginName'];
+	$mypassword=$_POST['ayipassword'];
+
+	// To protect MySQL injection
+	$myusername = stripslashes($myusername);
+	$mypassword = stripslashes($mypassword);
+	$myusername = mysql_real_escape_string($myusername);
+	$mypassword = mysql_real_escape_string($mypassword);
+
+	$sql="SELECT * FROM players WHERE name='$myusername' and password='$mypassword'";
+	$result=mysql_query($sql);
+
+	// Mysql_num_row is counting table row
+	$count=mysql_num_rows($result);
+
+	// If result matched $myusername and $mypassword, table row must be 1 row
+
+	if($count==1){
+		// Register $myusername, $mypassword and redirect to file "login_success.php"
+		session_register("myusername");
+		session_register("mypassword");
+		header("location:index.html");
+	}
+	else {
+		echo "Wrong Username or Password";
+	}
+?>
