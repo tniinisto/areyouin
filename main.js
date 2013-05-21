32d694c166e9d039681c735c981ad25dd65bbfb1
Tuomas <tniinisto@gmail.com>
2013-05-21 13:59:43 +0300

    asdf

--- main.js
+++ main.js
@@ -78,3 +78,17 @@ function getEvents() {
 	xmlhttp.send();
 }
 
+//Parse URL parameters
+function gup( name )
+{
+  name = name.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
+  var regexS = "[\\?&]"+name+"=([^&#]*)";
+  var regex = new RegExp( regexS );
+  var results = regex.exec( window.location.href );
+  if( results == null )
+    return "";
+  else
+    return results[1];
+}
+
+
