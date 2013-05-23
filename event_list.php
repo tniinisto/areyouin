158d7e8df9fb0b372b26ebbb8544de8fcec36b43
Tuomas <tniinisto@gmail.com>
2013-05-23 10:41:54 +0300

    asdf

--- event_list.php
+++ event_list.php
@@ -11,7 +11,7 @@
 	mysql_select_db("areyouin", $con);
 
 	//$sql="SELECT * FROM players";
-	$sql = "select v.Events_eventID, l.name as location,l.position as pos, e.startTime, e.endTime, p.name, p.photourl, v.areyouin, m.teamID, m.teamName, a.teamAdmin from events e, eventtype t, location l, players p,  eventplayer v, team m, playerteam a where t.eventTypeID = e.EventType_eventTypeID and l.locationID = e.Location_locationID and p.playerID = v.Players_playerID and v.Events_eventID = e.eventID and a.Players_playerID = p.playerID and a.Team_teamID = m.teamID and m.teamID = '" . $teamid  . "' order by e.startTime asc, v.Events_eventID asc";
+	$sql = "select v.Events_eventID, l.name as location,l.position as pos, e.startTime, e.endTime, p.playerid, p.name, p.photourl, v.areyouin, m.teamID, m.teamName, a.teamAdmin from events e, eventtype t, location l, players p,  eventplayer v, team m, playerteam a where t.eventTypeID = e.EventType_eventTypeID and l.locationID = e.Location_locationID and p.playerID = v.Players_playerID and v.Events_eventID = e.eventID and a.Players_playerID = p.playerID and a.Team_teamID = m.teamID and m.teamID = '" . $teamid  . "' order by e.startTime asc, v.Events_eventID asc";
 	 	
 	$result = mysql_query($sql);
 	
@@ -60,12 +60,13 @@
 		//Echo players for the event
 		echo "<table border='0' class=\"atable2\">";
 			echo "<tr>";
-				echo "<td class=\"col1\"><img width=\"40\" height=\"40\" src=\"images/" . $row['photourl'] . "\"></td>";
-				echo "<td class=\"col2\">" . $row['name'] . "</td>";
+				echo "<td class=\"col1\">" . $row['playerid'] . "</td>";
+				echo "<td class=\"col2\"><img width=\"40\" height=\"40\" src=\"images/" . $row['photourl'] . "\"></td>";
+				echo "<td class=\"col3\">" . $row['name'] . "</td>";
 				if($row['areyouin'] == 0)
-					echo "<td class=\"col3\">OUT</td>";
+					echo "<td class=\"col4\">OUT</td>";
 				else
-					echo "<td class=\"col3\">IN</td>";
+					echo "<td class=\"col4\">IN</td>";
 			echo "</tr>";
 		echo "</table>";		
 	}	
