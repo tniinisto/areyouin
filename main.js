bfa75acf384d94d70a33ab7271278788a3cee5c9
Tuomas <tniinisto@gmail.com>
2013-05-23 10:00:31 +0300

    sdf

--- main.js
+++ main.js
@@ -30,7 +30,8 @@ function getLoginInformation(playerID, teamID) {
 	}
 
 	//alert("GET ge7ts called.");
-	var variables = "playerid=" + playerID + "&teamid=" + teamID;
+	//var variables = "p=" + playerID + "&t=" + teamID;
+	var variables = "p=1&t=1";
 	//alert(variables);
 	xmlhttp.open("GET", "logininfo.php?", variables, false);
 	xmlhttp.send();
