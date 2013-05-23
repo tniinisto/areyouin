c21d992f40a0e5bf614296eccc8e4622f952c24f
Tuomas <tniinisto@gmail.com>
2013-05-23 10:11:19 +0300

    ada

--- logininfo.php
+++ logininfo.php
@@ -1,7 +1,7 @@
 <?php
 
-	$p=$_GET["p"];
-	$t=$_GET["t"];
+	$pl=$_GET["p"];
+	$te=$_GET["t"];
 
 	$con = mysql_connect('eu-cdbr-azure-north-a.cloudapp.net', 'bd3d44ed2e1c4a', '8ffac735');
 	if (!$con)
@@ -11,7 +11,7 @@
 
 	mysql_select_db("areyouin", $con);
 
-	$sql="SELECT p.name, t.teamName FROM players p, team t where p.playerID = '" . $p . "' and t.teamID = '" . $t "'";
+	$sql="SELECT p.name, t.teamName FROM players p, team t where p.playerID = " . $pl . " and t.teamID = " . $te;
 	
 	$result = mysql_query($sql);
 	$row = mysql_fetch_array($result);
