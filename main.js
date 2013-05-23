3fe1f53202b52215060f89023306c53d82a81498
Tuomas <tniinisto@gmail.com>
2013-05-23 09:51:36 +0300

    asdfa

--- main.js
+++ main.js
@@ -25,7 +25,7 @@ function getLoginInformation(playerID, teamID) {
 
 	xmlhttp.onreadystatechange = function () {
 		if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-			document.getElementById("user-login").innerHTML = xmlhttp.responseText;
+			document.getElementById("userlogin").innerHTML = xmlhttp.responseText;
 		}
 	}
 
