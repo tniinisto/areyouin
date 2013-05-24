c2b6b81e3059d4ad8c35a5eefbe60b96170f13c7
Tuomas <tniinisto@gmail.com>
2013-05-25 01:18:21 +0300

    adfa

--- events_sse.php
+++ events_sse.php
@@ -2,8 +2,8 @@
 	header('Content-Type: text/event-stream');
 	header('Cache-Control: no-cache');
 
-	$time = date('r');
-	echo "data: The server time is: {$time}\n\n";
+	//$time = date('r');
+	//echo "data: The server time is: {$time}\n\n";
 	
 	function sendMsg()
 	{
