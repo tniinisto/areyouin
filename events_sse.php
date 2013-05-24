eeac4402770402b435ba71973a942796e5f342f2
Tuomas <tniinisto@gmail.com>
2013-05-25 00:46:16 +0300

    sfs

--- events_sse.php
+++ events_sse.php
@@ -2,16 +2,14 @@
 	header('Content-Type: text/event-stream');
 	header('Cache-Control: no-cache');
 
-	//$time = date('r');
-	//echo "data: The server time is: {$time}\n\n";
+	$time = date('r');
+	echo "data: The server time is: {$time}\n\n";
 	
 	function sendMsg()
 	{
-		header('Content-Type: text/event-stream');
-		header('Cache-Control: no-cache');
-	
 		echo "event: ayi\n";
 		echo "data: event data changed\n\n";
+		//echo PHP_EOL;
 		ob_flush();
 		flush();
 	}
