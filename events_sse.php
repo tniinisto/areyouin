b07127d20ba0364c826b2650ac8961c43685846e
Tuomas <tniinisto@gmail.com>
2013-05-24 16:44:04 +0300

    dasdf

--- events_sse.php
+++ events_sse.php
@@ -5,10 +5,18 @@
 //$time = date('r');
 //echo "data: The server time is: {$time}\n\n";
 
-function notify()
+/*function notify()
 {
 	echo "data: event data changed\n\n";
 	flush();
+}*/
+
+function notify() {
+  echo "data: event data changed\n\n". PHP_EOL;
+
+  flush();
 }
+
+//sendMsg($serverTime, 'server time: ' . date("h:i:s", time()));
 
 ?> 
