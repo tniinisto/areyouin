ebb6730b891ae65f206bb11b1b525344cc1c70bf
Tuomas <tniinisto@gmail.com>
2013-05-24 15:13:24 +0300

    asdfa

--- events_sse.php
+++ events_sse.php
@@ -1,12 +1,14 @@
 <?php
-header('Content-Type: text/event-stream');
-header('Cache-Control: no-cache');
+
 
 //$time = date('r');
 //echo "data: The server time is: {$time}\n\n";
 
 function notify()
 {
+	header('Content-Type: text/event-stream');
+	header('Cache-Control: no-cache');
+
 	echo "data: event data changed\n\n";
 	flush();
 }
