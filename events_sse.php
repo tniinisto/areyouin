dea86ee8feaf4825b9863fd95d85d2778d93cb5b
Tuomas <tniinisto@gmail.com>
2013-05-24 16:56:00 +0300

    adfa

--- events_sse.php
+++ events_sse.php
@@ -5,17 +5,17 @@
 	//$time = date('r');
 	//echo "data: The server time is: {$time}\n\n";
 
-/*function notify()
-{
-	echo "data: event data changed\n\n";
-	flush();
-}*/
+	/*function notify()
+	{
+		echo "data: event data changed\n\n";
+		flush();
+	}*/
 
-/*function notify() {
-  echo "data: event data changed\n\n". PHP_EOL;
-  echo PHP_EOL;
-  ob_flush();
-  flush();
-}*/
+	function notifyEventUpdate() {
+	  echo "data: event data changed\n\n". PHP_EOL;
+	  echo PHP_EOL;
+	  ob_flush();
+	  flush();
+	}
 
 ?> 
