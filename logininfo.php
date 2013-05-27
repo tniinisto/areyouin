fd8f5d6914a7c22b36cca93bc35442d186f6a965
Tuomas <tniinisto@gmail.com>
2013-05-27 12:58:23 +0300

    asdfa

--- logininfo.php
+++ logininfo.php
@@ -11,13 +11,13 @@
 
 	mysql_select_db("areyouin", $con);
 
-	$sql="SELECT p.name, t.teamName FROM players p, team t where p.playerID = " . $pl . " and t.teamID = " . $te;
+	$sql="SELECT p.name, t.teamName, r.teamadmin FROM players p, team t, playerteam r where p.playerID = " . $pl . " and t.teamID = " . $te . " and r.players_playerid = " . $pl;
 	
 	$result = mysql_query($sql);
 	$row = mysql_fetch_array($result);
   
     //echo $sql;
-	echo "<font color=\"#ccc\">Welcome: </font>" . $row[name] . " <font color=\"#ccc\">Team: </font>" . $row[teamName];
+	echo "<font color=\"#ccc\">Welcome: </font>" . $row[name] . " <font color=\"#ccc\">Team: </font>" . $row[teamName] . "<style display=\"visible\">" . $row[teamadmin] . "</style>";
 	//echo "Welcome: " . $row[name] . " Team: </font>" . $row[teamname];
 	
 	mysql_close($con);
