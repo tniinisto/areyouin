609a0bd4c7ddad9e010bf1e8463368548b1d937a
Tuomas <tniinisto@gmail.com>
2013-05-28 21:21:09 +0300

    adfa

--- players_insert.php
+++ players_insert.php
@@ -17,10 +17,10 @@
 	while($row = mysql_fetch_array($result))
 	{
 		echo "<tr>";
-		echo "<td class=\"col1\">playerID " . $row['playerID'] . "</td>";
-		echo "<td class=\"icol1\"> <img width=\"40\" height=\"40\" src=\"images/" . $row['photourl'] . "\"></td>";
-		echo "<td class=\"icol2\">" . $row['name'] . "</td>";
-		echo "<td class=\"icol3\"> <input class=\"cb\" type=\"checkbox\" id=\"row" . $row_index . "\"<\input></td>";
+		echo "<td class=\"icol1\">playerID " . $row['playerID'] . "</td>";
+		echo "<td class=\"icol2\"> <img width=\"40\" height=\"40\" src=\"images/" . $row['photourl'] . "\"></td>";
+		echo "<td class=\"icol3\">" . $row['name'] . "</td>";
+		echo "<td class=\"icol4\"> <input class=\"cb\" type=\"checkbox\" id=\"row" . $row_index . "\"<\input></td>";
 		echo "</tr>";
 		
 		$row_index = $row_index + 1;
