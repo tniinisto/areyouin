0618ad4a2dac448d9e255f36e521a68d07dd6b79
Tuomas <tniinisto@gmail.com>
2013-05-28 14:48:11 +0300

    sdfa

--- players_insert.php
+++ players_insert.php
@@ -13,12 +13,12 @@
 	$result = mysql_query($sql);
   	
 	$row_index = 1; 
-	echo "<table border='1' id='insertplayers'>"; 
+	echo "<table border='1' id='insertplayers' class=\"atable2\">"; 
 	while($row = mysql_fetch_array($result))
 	{
 		echo "<tr>";
-		echo "<td>playerID " . $row['playerID'] . "</td>";
-		echo "<td> <img width=\"40\" height=\"40\" src=\"images/" . $row['photourl'] . "\"></td>";
+		echo "<td class=\"col1\">playerID " . $row['playerID'] . "</td>";
+		echo "<td class=\"col3\"> <img width=\"40\" height=\"40\" src=\"images/" . $row['photourl'] . "\"></td>";
 		echo "<td>" . $row['name'] . "</td>";
 		echo "<td> <input type=\"checkbox\" id=\"row" . $row_index . "\"</td>";
 		echo "</tr>";
