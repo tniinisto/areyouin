2b45fedfa07c7e14377a2b59048da61b07fa9bbf
Tuomas <tniinisto@gmail.com>
2013-05-29 10:15:02 +0300

    adfadf

--- insert_event.php
+++ insert_event.php
@@ -13,7 +13,10 @@
 	$gamestart=$_POST['gamestart'];
 	$gamesend=$_POST['gamesend'];
 
-	$players = array(); //(playerID, checkbox) values
+	$players1=$_POST['playerid1']; 
+	$players2=$_POST['ooswitch1']; 
+	
+	/*$players = array(); //(playerID, checkbox)
 	for ($i=1; $i<=$playeramount; $i++)
 	{
 		//echo "The number is " . $i . "<br>";
@@ -21,7 +24,7 @@
 		//$players[i][2] = $_POST["'onoffswitch" . $i ."'"];
 		$players[i][1] = $_POST['playerid1'];
 		$players[i][2] = $_POST['ooswitch1'];
-	}
+	}*/
 
 	// To protect MySQL injection
 	$playeramount = stripslashes($playeramount);
@@ -32,16 +35,17 @@
 	//$sql= "UPDATE eventplayer SET areyouin = '" . $areyouin . "' WHERE EventPlayerID = '".$eventplayerid."'";
 	
 	//echo $sql;
-
 	//$result = mysql_query($sql);
 	
 	echo "insert_event.php, playeamount: " . $playeramount . " start: " . $gamestart . " end: " . $gamesend;
 	echo "</br>";
-	for ($j=1; $j<=$playeramount; $j++)
+	echo "playerID: " . $players1 . " checkbox value: " . $players2;
+	
+	/*for ($j=1; $j<=$playeramount; $j++)
 	{
 		echo "playerID: " . $players[j][1] . " checkbox value: " . $players[j][2] . "";
 		echo "</br>";
-	}
+	}*/
 	
 	mysql_close($con);
 
