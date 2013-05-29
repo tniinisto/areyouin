728fc82d1c626ebaf4092e9b1458269d006c33b9
Tuomas <tniinisto@gmail.com>
2013-05-29 10:21:26 +0300

    adf

--- players_insert.php
+++ players_insert.php
@@ -18,9 +18,9 @@
 	echo "<form id=\"eventform\" method=\"post\" action=\"insert_event.php\">";
 	echo "<h2>Set Time</h2>";
 	echo "<label><h2>Game start:</h2></label>";
-	echo "<input type=\"datetime-local\" id=\"gamestart_id\" name=\"gamestart\" required></input>";
+	echo "<input type=\"datetime-local\" id=\"gamestart_id\" name=\"gamestart\" value=\"2013-05-30T00:59\" required></input>";
 	echo "<label><h2>Game end:</h2></label>";
-	echo "<input type=\"datetime-local\" id=\"gamesend_id\" name=\"gamesend\" required></input>";
+	echo "<input type=\"datetime-local\" id=\"gamesend_id\" name=\"gamesend\" value=\"2013-05-30T00:59\" required></input>";
 
 	$row_index = 1; 
 	echo "<table border='0' id='insertplayers' class=\"atable2\">"; 
@@ -28,8 +28,8 @@
 	while($row = mysql_fetch_array($result))
 	{
 		echo "<tr>";
-		echo "<td class=\"col1\"><input type=\"number\" name=\"playeramount\" value=\"" . $row_count . "\"</input></td>";
-		echo "<td class=\"col2\"><input type=\"number\" name=\"playerid" . $row_index . "\" value=\"" . $row['playerID'] . "\"</input></td>";
+		echo "<td class=\"col1\"><input type=\"number\" name=\"playeramount\" value=\"" . $row_count . "\"></input></td>";
+		echo "<td class=\"col2\"><input type=\"number\" name=\"playerid" . $row_index . "\" value=\"" . $row['playerID'] . "\"></input></td>";
 		echo "<td class=\"col3\"> <img width=\"40\" height=\"40\" src=\"images/" . $row['photourl'] . "\"></td>";
 		echo "<td class=\"col4\">" . $row['name'] . "</td>";
 		echo "<td class=\"col5\">";
