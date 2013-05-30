52cb5652014d781569c80bd295b692dc70929fa5
Tuomas <tniinisto@gmail.com>
2013-05-30 12:39:46 +0300

    afd

--- main.js
+++ main.js
@@ -205,7 +205,7 @@ function setSSE()
 //Event input form, copy start datetime to end
 function copyStart()
 {
-	var d = new Date(document.getElementById("gamestart_id").getAttribute("date"));
+	var d = new Date(document.getElementById("gamestart_id").value);
 	console.log(d);
 	document.getElementById("gameend_id").setAttribute("date", d.getDate());	
 }
