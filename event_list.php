8be7f7716e74d23248be40b6251b4ea1d8db1c2a
Tuomas <tniinisto@gmail.com>
2013-07-08 15:43:24 +0300

    seen

--- event_list.php
+++ event_list.php
@@ -64,7 +64,7 @@
 			if($row['seen'] == 0)
 				echo "<tr>";
 			else
-				echo "<tr bgcolor=\"#00FF00\">";
+				echo "<tr class=\"seen\">";
 				
 				echo "<td class=\"col1\">" . $row['eventplayerid'] . "</td>";
 				echo "<td class=\"col2\">" . $row['playerid'] . "</td>";
