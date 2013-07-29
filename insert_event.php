d4371e86c33ebf6131e106abda56ecdcdcb8f563
Tuomas <tniinisto@gmail.com>
2013-07-29 20:40:50 +0300

    dsfa

--- insert_event.php
+++ insert_event.php
@@ -111,7 +111,9 @@
 	//echo "</br>";
 	
 	if(isset($_SERVER['HTTP_REFERER'])) {
-		echo $_SERVER['HTTP_REFERER'];
+		$url = htmlspecialchars($_SERVER['HTTP_REFERER']);
+		//echo $_SERVER['HTTP_REFERER'];
+		header("Location: " . $url);
 	}
 	else
 	{
