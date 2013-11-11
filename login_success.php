9734cd33b4c5d67c3645c0dbca056147476c1695
Tuomas <tniinisto@gmail.com>
2013-11-11 17:50:16 +0200

    login routing back when not ok

--- login_success.php
+++ login_success.php
@@ -1,9 +1,11 @@
+<?php
+
 // Check if session is not registered, redirect back to main page. 
 // Put this code in first line of web page. 
-<?php
+
 session_start();
 if(!session_is_registered(myusername)){
-header("location:main_login.php");
+header("location:default.html");
 }
 ?>
 
