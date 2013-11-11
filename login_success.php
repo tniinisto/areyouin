caa58f6415c3c595dcde28d90eea0c3f444907be
Tuomas <tniinisto@gmail.com>
2013-11-11 18:21:24 +0200

    session variables

--- login_success.php
+++ login_success.php
@@ -11,7 +11,10 @@ else {
     //header("location:index.html");
     //header("location:index.html?userid=" . $row[playerID] . "&username=$myusername&teamid=" . $row[teamID] . "&teamname=" . $row[teamName]);
     echo "logged in with username";
-    echo ($_SESSION);
+    echo "<br>";
+    echo "<h3> PHP List All Session Variables</h3>";
+    foreach ($_SESSION as $key=>$val)
+        echo $key." ".$val."<br/>";
     echo "<br>";
 }
 
