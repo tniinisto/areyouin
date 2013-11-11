0d1858865bbdded15279b07e4ba72cbc20416801
Tuomas <tniinisto@gmail.com>
2013-11-11 18:40:51 +0200

    session

--- login_success.php
+++ login_success.php
@@ -3,7 +3,7 @@
 // Check if session is not registered, redirect back to main page. 
 // Put this code in first line of web page. 
 
-//session_start();
+session_start();
 
 if(!session_is_registered(myusername)){
 header("location:default.html");
