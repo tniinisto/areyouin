2a832a4797adb05a4486a11bba00cf291ba1cdc2
Tuomas <tniinisto@gmail.com>
2013-12-22 01:01:34 +0200

    update_event start&end time handling for datetime-local input (Chrome exception with format)

--- update_event.php
+++ update_event.php
@@ -1,13 +1,16 @@
 <?php
+        include 'ChromePhp.php';
+        ChromePhp::log('Hello console!');
+
         $eventid=$_GET["eventid"];
         
-        //session_start();
+        session_start();
 	
         //$teamid=$_GET["teamid"];
 	    //$playerid=$_GET["playerid"];
 
         //$playerid=$_SESSION['myplayerid'];
-	    //$teamid=$_SESSION['myteamid'];
+	    $teamid=$_SESSION['myteamid'];
 
         //echo "update_event.php called eventid=" . $eventid;
 
@@ -19,13 +22,70 @@
 
         mysql_select_db("areyouin", $con)or die("cannot select DB");
 
+        /*Eventin tiedot ja siinä jo olevat tiimin pelaajat*/
+        $sql = "select e.eventID, p.playerID, p.name, e.startTime, e.endTime, l.name location from areyouin.eventplayer ep
+        inner join areyouin.events e on e.eventID = ep.Events_eventID
+        inner join areyouin.team t on teamID = e.Team_teamID
+        inner join areyouin.players p on playerID = ep.Players_playerID
+        inner join areyouin.location l on l.locationID = e.Location_locationID
+        where e.eventID = " . $eventid . " and t.teamID = " . $teamid . "";
+        
+        $result = mysql_query($sql);
+        $row = mysql_fetch_array($result);
+
+        $user_agent = $_SERVER['HTTP_USER_AGENT'];
+        //ChromePhp::log($user_agent);
+        
+        //Datetime format for Chrome (Add "T")
+        $gamestart = date(('Y-m-d H:i'), strtotime($row['startTime']));
+        $gamestart[10] = "T";
+        $gameend = date(('Y-m-d H:i'), strtotime($row['endTime']));
+        $gameend[10] = "T";
+
+         //Debug
+        //echo($sql);
+        //ChromePhp::log("startTime:");
+        //ChromePhp::log($row['startTime']);
+        //ChromePhp::log("endTime:");
+        //ChromePhp::log($row['endTime']);
+        //ChromePhp::log($gamestart);
+        //$gamestart = str_replace("-", "T", $row['startTime']);
+        //ChromePhp::log(date(('Y-m-d H:i'), strtotime($row['startTime'])));
+        ////ChromePhp::log(date(('Y-m-d H:i'), strtotime(strtotime('+10 hours'))));
+        //ChromePhp::log(date(('Y-m-d H:i'), strtotime(strtotime($gamestart))));
+
+
+        //Get team's players
+        //$sql="SELECT p.playerID, p.name, p.photourl FROM players p, team t WHERE t.teamID = '" . $teamid . "'";);
+        //$result = mysql_query($sql);
+        //$row_count = mysql_num_rows($result);
+
+        //Get event data
+        //$sql2 = "";
+        //$result2 = mysql_query($sql2);
+
         echo "<article id=\"event_content_article\" class=\"clearfix \">";
         echo "<h1>Update game</h1>";
         echo "<form id=\"eventupdateform\" method=\"post\" action=\"update_event_db.php\">";
         echo "<label><h2>Game start:</h2></label>";
-        echo "<input type=\"datetime-local\" id=\"gamestart_id\" name=\"gamestart\" required value=\"" . date(('Y-m-d H:i'), strtotime('+10 hours')) . "\"></input>";
+        //echo "<input type=\"datetime-local\" id=\"gamestart_id\" name=\"gamestart\" required value=\"" . date(('Y-m-d H:i'), strtotime('+10 hours')) . "\"></input>";
+        //echo "<input type=\"datetime-local\" id=\"gamestart_id\" name=\"gamestart\" required value=\"" . date(('Y-m-d H:i'), strtotime($row['startTime'])) . "\"></input>";
+        if (preg_match('/chrome/i', $user_agent)) {
+            echo "<input type=\"datetime-local\" id=\"gamestart_id\" name=\"gamestart\" required value=\"" . $gamestart . "\"></input>";
+        }
+        else {
+            echo "<input type=\"datetime-local\" id=\"gamestart_id\" name=\"gamestart\" required value=\"" . date(('Y-m-d H:i'), strtotime($row['startTime'])) . "\"></input>";
+        }
+
         echo "<label><h2>Game end:</h2></label>";
-        echo "<input type=\"datetime-local\" id=\"gameend_id\" name=\"gamesend\" required value=\"" . date(('Y-m-d H:i'), strtotime('+12 hours')) . "\"></input>";
+        //echo "<input type=\"datetime-local\" id=\"gameend_id\" name=\"gamesend\" required value=\"" . date(('Y-m-d H:i'), strtotime('+12 hours')) . "\"></input>";
+        //echo "<input type=\"datetime-local\" id=\"gameend_id\" name=\"gamesend\" required value=\"" . date(('Y-m-d H:i'), strtotime($row['endTime'])) . "\"></input>";
+        if (preg_match('/chrome/i', $user_agent)) {
+            echo "<input type=\"datetime-local\" id=\"gameend_id\" name=\"gamesend\" required value=\"" . $gameend . "\"></input>";
+        }
+        else {
+            echo "<input type=\"datetime-local\" id=\"gameend_id\" name=\"gamesend\" required value=\"" . date(('Y-m-d H:i'), strtotime($row['endTime'])) . "\"></input>";
+        }
 
         echo "<h2>Pick players:</h2>";
         $row_index = 1; 
