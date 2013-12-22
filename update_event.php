0ec21d7cd3dde453524a4bd25d10648f7e5fdd9a
Tuomas <tniinisto@gmail.com>
2013-12-22 10:46:24 +0200

    event update: lists players and tags players that are already in

--- update_event.php
+++ update_event.php
@@ -54,7 +54,6 @@
         ////ChromePhp::log(date(('Y-m-d H:i'), strtotime(strtotime('+10 hours'))));
         //ChromePhp::log(date(('Y-m-d H:i'), strtotime(strtotime($gamestart))));
 
-
         //Get team's players
         //$sql="SELECT p.playerID, p.name, p.photourl FROM players p, team t WHERE t.teamID = '" . $teamid . "'";);
         //$result = mysql_query($sql);
@@ -64,6 +63,7 @@
         //$sql2 = "";
         //$result2 = mysql_query($sql2);
 
+        //Form - Start & End time/////////////////////
         echo "<article id=\"event_content_article\" class=\"clearfix \">";
         echo "<h1>Update game</h1>";
         echo "<form id=\"eventupdateform\" method=\"post\" action=\"update_event_db.php\">";
@@ -86,10 +86,99 @@
         else {
             echo "<input type=\"datetime-local\" id=\"gameend_id\" name=\"gamesend\" required value=\"" . date(('Y-m-d H:i'), strtotime($row['endTime'])) . "\"></input>";
         }
-
+        
+        //Form - Players////////////////////////////////////
         echo "<h2>Pick players:</h2>";
+
+        mysql_data_seek($result, 0); //Reset $result index position (earlier query)
+        $eventplayers = array();
+        $index = 0;
+        while($row = mysql_fetch_array($result))
+        {
+            $eventplayers[$index] = $row['playerID'];
+            //ChromePhp::log("Array: " . $index . ": " . $eventplayers[$index]);
+            $index++;
+        }
+
+        $sql2="SELECT p.playerID, p.name, p.photourl FROM players p, team t WHERE t.teamID = '" . $teamid . "'";
+        
+        $result2 = mysql_query($sql2);
+        $row_count = mysql_num_rows($result2);
+
         $row_index = 1; 
-        /*echo "<table border='0' id='insertplayers' class=\"atable2\">"; 
+        echo "<table border='0' id='insertplayers' class=\"atable2\">"; 
+        
+        while($row2 = mysql_fetch_array($result2))
+        {
+                //echo "<div style=\"display: inline-block;\">";
+                    
+                    echo "<tr>";
+                    echo "<td class=\"pcol1\"><input type=\"number\" name=\"playeramount\" value=\"" . $row_count . "\"></input></td>";
+                    echo "<td class=\"pcol2\"><input type=\"number\" name=\"playerid" . $row_index . "\" value=\"" . $row2['playerID'] . "\"></input></td>";
+                    echo "<td class=\"pcol3\"> <img width=\"40\" height=\"40\" src=\"images/" . $row2['photourl'] . "\"></td>";
+                    echo "<td class=\"pcol4\">" . $row2['name'] . "</td>";
+                    echo "<td class=\"pcol5\">";
+                            if(in_array($row2['playerID'], $eventplayers)) {
+                                echo "<div class=\"onoffswitch\">";
+                                        echo "<input type=\"checkbox\" name=\"ooswitch" . $row_index . "\" class=\"onoffswitch-checkbox\" id=\"myonoff" . $row_index . "\">";
+                                        echo "<label class=\"onoffswitch-label\" for=\"myonoff" . $row_index . "\">";
+                                        echo "<div class=\"onoffswitch-inner\"></div>";
+                                        echo "<div class=\"onoffswitch-switch\"></div>";
+                                        echo "</label>";
+                                echo "</div>";
+                            }
+                            else {
+                                echo "<div class=\"onoffswitch\">";
+                                        echo "<input type=\"checkbox\" name=\"ooswitch" . $row_index . "\" class=\"onoffswitch-checkbox\" id=\"myonoff" . $row_index . "\" checked>";
+                                        echo "<label class=\"onoffswitch-label\" for=\"myonoff" . $row_index . "\">";
+                                        echo "<div class=\"onoffswitch-inner\"></div>";
+                                        echo "<div class=\"onoffswitch-switch\"></div>";
+                                        echo "</label>";
+                                echo "</div>";                                
+                            }
+                    echo "</td>";
+                    
+                    if($row2 = mysql_fetch_array($result2))
+                    {
+                        $row_index = $row_index + 1;
+                        echo "<td style=\"width: 20px;\"></td>";
+                        echo "<td class=\"pcol1\"><input type=\"number\" name=\"playeramount\" value=\"" . $row_count . "\"></input></td>";
+                        echo "<td class=\"pcol2\"><input type=\"number\" name=\"playerid" . $row_index . "\" value=\"" . $row2['playerID'] . "\"></input></td>";
+                        echo "<td class=\"pcol3\"> <img width=\"40\" height=\"40\" src=\"images/" . $row2['photourl'] . "\"></td>";
+                        echo "<td class=\"pcol4\">" . $row2['name'] . "</td>";
+                        echo "<td class=\"pcol5\">";
+                            if(in_array($row2['playerID'], $eventplayers)) {
+                                echo "<div class=\"onoffswitch\">";
+                                        echo "<input type=\"checkbox\" name=\"ooswitch" . $row_index . "\" class=\"onoffswitch-checkbox\" id=\"myonoff" . $row_index . "\">";
+                                        echo "<label class=\"onoffswitch-label\" for=\"myonoff" . $row_index . "\">";
+                                        echo "<div class=\"onoffswitch-inner\"></div>";
+                                        echo "<div class=\"onoffswitch-switch\"></div>";
+                                        echo "</label>";
+                                echo "</div>";
+                            }
+                            else {
+                                echo "<div class=\"onoffswitch\">";
+                                        echo "<input type=\"checkbox\" name=\"ooswitch" . $row_index . "\" class=\"onoffswitch-checkbox\" id=\"myonoff" . $row_index . "\" checked>";
+                                        echo "<label class=\"onoffswitch-label\" for=\"myonoff" . $row_index . "\">";
+                                        echo "<div class=\"onoffswitch-inner\"></div>";
+                                        echo "<div class=\"onoffswitch-switch\"></div>";
+                                        echo "</label>";
+                                echo "</div>";                                
+                            }
+                        echo "</td>";
+                    }                                      
+                    echo "</tr>";
+                
+                //echo "</div>";
+
+                $row_index = $row_index + 1;
+        }
+        echo "</table>";
+        echo "</br>";
+        echo "</br>";
+
+        //$row_index = 1; 
+        /*echo "<table border='0' id='insertplayers' class=\"atable2\">";
         
         while($row = mysql_fetch_array($result))
         {
