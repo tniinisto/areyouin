53705b59db14010e0e0c33ee780500e7a00a5c39
Tuomas <tniinisto@gmail.com>
2014-01-21 20:33:17 +0200

    sse 4 minutes

--- events_sse.php
+++ events_sse.php
@@ -7,7 +7,7 @@
         echo "event: ayi\n";
         //echo "data: The server time is: {$time}\n\n";
         echo "data: e\n\n";
-        sleep(300);
+        sleep(240);
         
         /*function sendMsg()
         {
