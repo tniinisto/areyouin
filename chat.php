0e8899ed3613277f685bec9a10653aefe71f5ad6
Tuomas <tniinisto@gmail.com>
2014-04-21 09:21:29 +0300

    chat...

--- chat.php
+++ chat.php
@@ -1,25 +1,38 @@
 <?php
 
     session_start();
+    
+    //include 'ChromePhp.php';        
+    //ChromePhp::log("starting chat...");
 
-    //$playerid=$_SESSION['myplayerid'];
-	$teamid=$_SESSION['myteamid'];
-    //$ad=$_SESSION['myAdmin'];
+    if(isset($_POST['sendbutton'])) {
+       sendComment();
+    } 
+    else {
 
-	$con = mysql_connect('eu-cdbr-azure-north-a.cloudapp.net', 'bd3d44ed2e1c4a', '8ffac735');
-	if (!$con)
-	  {
-	  die('Could not connect: ' . mysql_error());
-	  }
+        //$playerid=$_SESSION['myplayerid'];
+	    $teamid=$_SESSION['myteamid'];
 
-	mysql_select_db("areyouin", $con);
+	    $con = mysql_connect('eu-cdbr-azure-north-a.cloudapp.net', 'bd3d44ed2e1c4a', '8ffac735');
+	    if (!$con)
+	      {
+	      die('Could not connect: ' . mysql_error());
+	      }
 
-    $sql = "SELECT * FROM comments WHERE team_teamID = " . $teamid . "";
+	    mysql_select_db("areyouin", $con);
 
-    $result = mysql_query($sql);
-    $row = mysql_fetch_array($result);
+        $sql = "SELECT * FROM comments WHERE team_teamID = " . $teamid . "";
 
-    //echo "PHP comment: " . $row['comment'] . ""; 
+        $result = mysql_query($sql);
+        $row = mysql_fetch_array($result);
+
+    }
+
+
+    function sendComment() {
+        //ChromePhp::log("send comment...");
+    }
+
 ?>
 
 <!DOCTYPE html>
@@ -33,19 +46,24 @@
         <article id="chat_content_article" class="clearfix">
             
             <!--<div id="chat_content_id">-->        
-                <h4>Testing chat document...</h4>
-                <!--<h4>Comment:</h4>-->
-                <a id="chattest" href="#">Chat</a>
-
-                <?php
-                    echo "<h4>PHP Comment: " .  $row['comment'] . "</h4>";
-                ?>
-
-                <!--<div id="output"></div>-->
-                  
+                <!--<h4>Testing chat document...</h4>
+                <h4>Comment:</h4>
+                <a id="chattest" href="#">Chat</a>-->
             <!--</div>-->
 
+            <?php
+                echo "<h4>PHP Comment: " .  $row['comment'] . "</h4>";
+                
+                echo "<form id=\"chatform\" name=\"chatform\" method=\"post\" action=\"". $_SERVER[PHP_SELF] ."\" target=\"frame_chat\">";
+                    echo "<label for=\"comment\">Text: </label>";
+			        echo "<input type=\"text\" id=\"login\" name=\"comment\" placeholder=\"\" required>";
+                    echo "<input type=\"submit\" value=\"Send\" name=\"sendbutton\" id=\"sendbutton\">";
+		        echo "</form>";
+            ?>
+                  
         </article>
+
+        <iframe name="frame_chat" style="display: none;"></iframe>
         
     </body>
 </html>
