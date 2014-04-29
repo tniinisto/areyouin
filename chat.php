46c8b9f5d06464fe39d03c7f486fc47cb292f358
Tuomas <tniinisto@gmail.com>
2014-04-29 19:17:05 +0300

    elastiiiccc

--- chat.php
+++ chat.php
@@ -91,9 +91,9 @@
                                 echo "<td width=\"500px\" height=\"auto\"><text class=\"commentArea1\">" . $published->format("j.n.Y H:i") . "</text><textarea readonly class=\"commentArea2\" id=\"area" . $i ."\">" . $row['comment'] . "</textarea></td>";
                             echo "</tr>";
 
-                            //echo "<script>";
-                            //echo "$(\"#area" . $i . "\").elastic();";
-                            //echo "</script>";
+                            echo "<script>";
+                            echo "$(\"#area" . $i . "\").elastic();";
+                            echo "</script>";
 
                             $i++;
                         }
