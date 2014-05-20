6eaeb82b0dc8be61c5a3ba365167435fa2accef0
Tuomas <tniinisto@gmail.com>
2014-05-20 15:44:16 +0300

    chat inline & kinetic 1

--- chat.php
+++ chat.php
@@ -82,7 +82,7 @@
 
             <div id="chatdiv" class="scrollit">
                 <!--<p style="display:none;">Just to enable webkit-overflow-scrolling: touch</p>-->
-                <table id="comments_table" class="atable" border="0" style="display: inline-table;">
+                <table id="comments_table" class="atable" border="0" style="display: inline; webkit-overflow-scrolling: touch;">
                     <?php
                         
                     $limit=30;
