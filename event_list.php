7b3ffb2d3774d8178202604a76654d539262da78
Tuomas <tniinisto@gmail.com>
2014-10-26 10:33:38 +0200

    Kenttä paketissa

--- event_list.php
+++ event_list.php
@@ -260,6 +260,7 @@
                 echo "<div>";
 
                     //echo "<h3 style=\"text-align: center;\">No games currently scheduled...</h3>";
+
                     echo "<h3 style=\"text-align: center;\">Kenttä paketissa kauden 2014 osalta, kiitokset peleistä!</h3>";
 
                 echo "</div>";
