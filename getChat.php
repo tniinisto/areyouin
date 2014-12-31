4641e81b53e4cc5f14c5d2a8f11de8fe84c41b15
Tuomas <tniinisto@gmail.com>
2014-12-31 14:17:39 +0200

    timezone to helsinki

--- getChat.php
+++ getChat.php
@@ -1,6 +1,6 @@
 <?php
     session_start();
-    date_default_timezone_set('UTC');
+    date_default_timezone_set('Europe/Helsinki');
         
     if($_SESSION['ChromeLog']) {
         require_once 'ChromePhp.php';
