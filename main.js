379879fb72f4fb354a0f9f536486828406b95e5a
Tuomas <tniinisto@gmail.com>
2015-06-27 16:09:01 +0300

    test publish

--- main.js
+++ main.js
@@ -541,7 +541,9 @@ function toLoginPage() {
     var loginURL = window.location.href;
     loginURL = loginURL.substring(0, loginURL.lastIndexOf('/') + 1);
     loginURL = loginURL + "default.html";
+    
     //alert(loginURL);
+
     window.location.assign(loginURL);
     //window.location.assign("http://m-areyouin.azurewebsites.net/default.html");
     //window.location.assign("http://localhost:18502/default.html")    
