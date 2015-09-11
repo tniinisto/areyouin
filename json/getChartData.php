834dfc1753ca2bd0a109bc442fb3d3f50731fc78
Tuomas <tniinisto@gmail.com>
2015-09-11 10:38:51 +0300

    chartdate sql for team and player

--- json/getChartData.php
+++ json/getChartData.php
@@ -10,6 +10,22 @@
                         where Team_teamID = " . $teamid . "
                         GROUP BY YEAR(events.startTime), MONTH(events.startTime);";
 
+    //SELECT year1, month1, games_set, participated
+    //FROM
+
+    //(SELECT eventID, YEAR(e1.startTime) as year1, MONTH(e1.startTime) as month1, count(e1.eventID) 'games_set'
+    //FROM `areyouin`.`events` e1
+    //where Team_teamID = 1
+    //GROUP BY YEAR(e1.startTime), MONTH(e1.startTime)) t1
+    //left join
+
+    //(SELECT eventID, YEAR(e2.startTime) as year2, MONTH(e2.startTime) as month2, count(ep.eventPlayerID) as participated
+    //FROM eventplayer ep, events e2
+    //where ep.Players_playerID = 1 and ep.areyouin = 1 and e2.eventID = ep.Events_eventID
+    //GROUP BY YEAR(e2.startTime), MONTH(e2.startTime)) t2
+
+    //on t1.eventID = t2.eventID;
+
     
     try {
         //PDO means "PHP Data Objects"
@@ -18,20 +34,29 @@
 
 	    $dbh = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);	
 
-	    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	    //Team's events///////////////////////////////////////////////////////////////////////
+        $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	    $stmt = $dbh->query($sql_team_events);  
 	    //$result = $stmt->fetchAll(PDO::FETCH_OBJ);
 
-        $chart_data_table = array();
+        $team_data_table = array();
         if ($stmt->execute()) {
             while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-                $chart_data_table[] = $row;
+                $team_data_table[] = $row;
             }
         }
 
-        echo '{"items":'. json_encode($chart_data_table) .'}'; 
+        echo '{"items":'. json_encode($team_data_table) .'}'; 
 
 	    $dbh = null;
+        //Team's events///////////////////////////////////////////////////////////////////////
+
+
+        //Player's events/////////////////////////////////////////////////////////////////////
+        
+        //Player's events/////////////////////////////////////////////////////////////////////
+
+
 
     }
     catch(PDOException $e) {
