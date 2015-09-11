be8d0e3860a55a441b5dd5ddc64aebcf1fa80099
Tuomas <tniinisto@gmail.com>
2015-09-11 16:02:29 +0300

    tune

--- json/chartdata.js
+++ json/chartdata.js
@@ -23,8 +23,8 @@ function getChartData() {
         //alert("FIRST ROW month/year: " + chartdata[0].month + "/" + chartdata[0].year + " Your games: " + chartdata[0].participated  + " Games set: " + chartdata[0].games +
         //" SECOND ROW month/year: " + chartdata[1].month+ "/" + chartdata[1].year  + " Your games: " + chartdata[1].participated  + " Games set: " + chartdata[1].games);
 
-        alert("FIRST ROW month/year: " + chartdata[0].month + " Your games: " + chartdata[0].participated  + " Games set: " + chartdata[0].games +
-        " SECOND ROW month/year: " + chartdata[1].month + " Your games: " + chartdata[1].participated  + " Games set: " + chartdata[1].games);
+        alert("FIRST ROW year/month: " + chartdata[0].month + " Your games: " + chartdata[0].participated  + " Games set: " + chartdata[0].games +
+        " SECOND ROW year/month: " + chartdata[1].month + " Your games: " + chartdata[1].participated  + " Games set: " + chartdata[1].games);
 
 
         //$.each(playerinfo, function(index, player) {
