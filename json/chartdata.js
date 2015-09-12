0db6f75f52ebd3d3d7ed01c18ce66a85ce50becd
Tuomas <tniinisto@gmail.com>
2015-09-12 18:55:47 +0300

    test

--- json/chartdata.js
+++ json/chartdata.js
@@ -23,9 +23,12 @@ function getChartData() {
         alert("FIRST ROW year/month: " + chartdata[0].month + " Your games: " + chartdata[0].participated + " Games set: " + chartdata[0].games +
         " SECOND ROW year/month: " + chartdata[1].month + " Your games: " + chartdata[1].participated + " Games set: " + chartdata[1].games);
 
-        var games_chart_array = new Array();
+        var games_chart_array;
+        games_chart_array[0][0] = 'Month';
+        games_chart_array[0][1] = 'Your games';
+        games_chart_array[0][2] = 'Games set';
 
-        for (i = 0; i < chartdata.length; i++) {
+        for (i = 1; i < chartdata.length; i++) {
             games_chart_array[i][0] = chartdata[i].month;
             games_chart_array[i][1] = chartdata[i].participated;
             games_chart_array[i][1] = chartdata[i].games;
