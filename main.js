43b2aae6d8f495dbbdfc6c249f569d839338affe
Tuomas <tniinisto@gmail.com>
2015-09-18 17:55:31 +0300

    rain tune

--- main.js
+++ main.js
@@ -937,7 +937,7 @@ function createRain() {
 
 	for( i=1;i<nbDrop;i++) {
 	var dropLeft = randRange(0,1600);
-	var dropTop = randRange(-1000,1400);
+	var dropTop = randRange(-1000,700);
 
 	$('.rain').append('<div class="drop" id="drop'+i+'"></div>');
 	$('#drop'+i).css('left',dropLeft);
