afa6e3335d561f2807ba1cfc4f42a2643efa43f3
Tuomas <tniinisto@gmail.com>
2015-09-18 19:17:04 +0300

    rain tune

--- main.js
+++ main.js
@@ -923,7 +923,7 @@ function drawChart() {
     }, 200);
 }
 
-//Rain
+//Rain/////////////////////////////////////////////////////////////////
 // number of drops created.
 var nbDrop = 258; 
 
@@ -937,7 +937,7 @@ function createRain() {
 
 	for( i=1;i<nbDrop;i++) {
 	var dropLeft = randRange(0, screen.width - 150);
-	var dropTop = randRange(-1000, 600);
+	var dropTop = randRange(-40000, 600);
 
 	$('.rain').append('<div class="drop" id="drop'+i+'"></div>');
 	$('#drop'+i).css('left',dropLeft);
