e5b76957995380e511b686b03dc7cf50624dc74c
Tuomas <tniinisto@gmail.com>
2015-09-24 18:42:01 +0300

    tune

--- event_list.php
+++ event_list.php
@@ -140,8 +140,9 @@
                     echo "<table class=\"atable\">";			    
             	        echo "<tr>";
 					        echo "<th> Games @&nbsp 
-                            <img id='gmaps_". $row_index . "' width='15' height='15' src='images/googlemaps.jpg' style='cursor: pointer'></img>
                             <a href='https://maps.google.fi/maps?q=" . $row['pos'] . "&npsp target='_blank\'>" . $row['location'] . "
+                            &nbsp
+                            <img id='gmaps_". $row_index . "' width='15' height='15' src='images/googlemaps.jpg' style='cursor: pointer'></img>
                             </a></th>";
 				        echo "</tr>";
 			        echo "</table>";
