60ef1ccab308942215ab25f0d9af4d27133f261e
Tuomas <tniinisto@gmail.com>
2015-09-26 13:37:15 +0300

    test

--- main.js
+++ main.js
@@ -382,8 +382,9 @@ function game_end() {
 //Check password change
 function check_pass() {
     alert("jes");
+    document.getElementById("dialog_password2").value = "";
     if (document.getElementById("dialog_password1").value != document.getElementById("dialog_password2").value)
-        document.getElementById("dialog_password2").value = '';
+        document.getElementById("dialog_password2").value = "";
 }
 
 //Player profile
