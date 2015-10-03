78d8a401576663f6df77ad0a6a36cc84cea6b1e6
Tuomas <tniinisto@gmail.com>
2015-10-03 10:06:33 +0300

    test default location

--- players_insert.php
+++ players_insert.php
@@ -52,6 +52,7 @@
                     $result2 = mysql_query($sql2);
 
                     echo "<select id=\"location_select\" name=\"location\" form=\"eventform\">";
+                    echo "<option value='0'> Not defined </option>"; //Default location when no locations entered to RYouIN
                     while($row2 = mysql_fetch_array($result2))
 	                {  
                         echo "<option value=\"" . $row2['locationID'] . "\">" . $row2['name'] . "</option>";
