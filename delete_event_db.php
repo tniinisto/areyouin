9964cd50062cc0f94392d11fe4886518f721a680
Tuomas <tniinisto@gmail.com>
2015-10-10 19:55:51 +0300

    pdo delete

--- delete_event_db.php
+++ delete_event_db.php
@@ -11,9 +11,21 @@
 
     mysql_select_db("areyouin", $con)or die("cannot select DB");
             
-    $eventid=$_POST['update_eventid'];
-    $teamid=$_POST['update_teamid'];
+    //$eventid=$_POST['delete_eventid'];
     
+    $sql1 = "DELETE FROM eventplayer WHERE Events_eventID =  :eventId";
+    $stmt = $pdo->prepare($sql1);
+    $stmt->bindParam(':eventId', $_POST['delete_eventid'], PDO::PARAM_INT);   
+    $stmt->execute();
+
+    $sql2 = "DELETE FROM events WHERE eventID =  :eventId";
+    $stmt = $pdo->prepare($sql2);
+    $stmt->bindParam(':eventId', $_POST['delete_eventid'], PDO::PARAM_INT);   
+    $stmt->execute();
+
+    header("location:index.html");    
+    
+
 ?>
 
 
