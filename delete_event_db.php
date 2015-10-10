fafb87c248af6c5cc73c9f09af3628e17b418d88
Tuomas <tniinisto@gmail.com>
2015-10-10 20:09:44 +0300

    pdo syntax

--- delete_event_db.php
+++ delete_event_db.php
@@ -3,24 +3,21 @@
     //include 'ChromePhp.php';
     //ChromePhp::log('Hello console!');
 
-    $con = mysql_connect($dbhost, $dbuser, $dbpass);
-    if (!$con)
-        {
-        die('Could not connect: ' . mysql_error());
-        }
-
-    mysql_select_db("areyouin", $con)or die("cannot select DB");
+  	$dbh = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);	
+	$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             
     try {    
-        //$sql1 = "DELETE FROM eventplayer WHERE Events_eventID = :eventId";
-        //$stmt1 = $pdo->prepare($sql1);
-        //$stmt1->bindParam(':eventId', $_POST['delete_eventid'], PDO::PARAM_INT);   
-        //$stmt1->execute();
+        $sql1 = "DELETE FROM eventplayer WHERE Events_eventID = :eventId";
+        $stmt1 = $dbh->prepare($sql1);
+        $stmt1->bindParam(':eventId', $_POST['delete_eventid'], PDO::PARAM_INT);   
+        $stmt1->execute();
 
-        //$sql2 = "DELETE FROM events WHERE eventID = :eventId";
-        //$stmt2 = $pdo->prepare($sql2);
-        //$stmt2->bindParam(':eventId', $_POST['delete_eventid'], PDO::PARAM_INT);   
-        //$stmt2->execute();
+        $sql2 = "DELETE FROM events WHERE eventID = :eventId";
+        $stmt2 = $dbh->prepare($sql2);
+        $stmt2->bindParam(':eventId', $_POST['delete_eventid'], PDO::PARAM_INT);   
+        $stmt2->execute();
+
+        $dbh = null;
     }
     catch(PDOException $e) {
 	    echo '{"error":{"text":'. $e->getMessage() .'}}'; 
