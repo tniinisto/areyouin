d4b58e74883949f6334332ae3d746d266d767e22
Tuomas <tniinisto@gmail.com>
2015-10-17 16:04:51 +0300

    tune

--- players_insert.php
+++ players_insert.php
@@ -239,15 +239,6 @@
                             echo "<td class=''>playerID: " . $row['playerID'] . "</td>";
                             echo "<td class=''><img width='40' height='40' src='images/" . $row['photourl'] . "'></td>";
                             echo "<td class=''>" . $row['name'] . "</td>";
-                            //echo "<td class=''>";
-                            //        echo "<div class='onoffswitch'>";
-                            //                echo "<input type='checkbox' name='ooswitch" . $row_index . "' class='onoffswitch-checkbox' id='myonoff" . $row_index . "' checked>";
-                            //                echo "<label class='onoffswitch-label' for='myonoff" . $row_index . "'>";
-                            //                echo "<div class='onoffswitch-inner'></div>";
-                            //                echo "<div class='onoffswitch-switch'></div>";
-                            //                echo "</label>";
-                            //        echo "</div>";
-                            //echo "</td>";
                         echo "<tr>";
                         echo "<br>";
                     }
