30065d16e5f8b3347c098d4ac33a8d59ceb4b96f
Tuomas <tniinisto@gmail.com>
2015-10-24 18:40:03 +0300

    tune

--- event_list.php
+++ event_list.php
@@ -43,7 +43,7 @@
         inner join team t on t.teamID = pt.Team_teamID
         where t.teamID = '" . $teamid  . "' and e.Team_teamID = t.teamID
         and (e.endTime - INTERVAL " . $_SESSION['myoffset'] . " HOUR) > now()
-        order by e.startTime asc, ep.Events_eventID asc, ep.areyouin desc, ep.seen desc limit" . MAX_NRO_EVENTS . "";
+        order by e.startTime asc, ep.Events_eventID asc, ep.areyouin desc, ep.seen desc limit " . MAX_NRO_EVENTS . "";
 
 	    $result = mysql_query($sql);
 	
