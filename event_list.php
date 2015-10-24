842d490cbc6a0cba52f240894f5f62f2bf8da96b
Tuomas <tniinisto@gmail.com>
2015-10-24 18:38:25 +0300

    go

--- event_list.php
+++ event_list.php
@@ -43,8 +43,7 @@
         inner join team t on t.teamID = pt.Team_teamID
         where t.teamID = '" . $teamid  . "' and e.Team_teamID = t.teamID
         and (e.endTime - INTERVAL " . $_SESSION['myoffset'] . " HOUR) > now()
-        limit 10
-        order by e.startTime asc, ep.Events_eventID asc, ep.areyouin desc, ep.seen desc";
+        order by e.startTime asc, ep.Events_eventID asc, ep.areyouin desc, ep.seen desc limit 10";
 
 	    $result = mysql_query($sql);
 	
