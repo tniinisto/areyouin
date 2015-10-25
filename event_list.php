7f987e581520e0b75e5e6a44494e3224fa0b9477
Tuomas <tniinisto@gmail.com>
2015-10-25 18:43:01 +0200

    test

--- event_list.php
+++ event_list.php
@@ -42,7 +42,8 @@
         //    $time_condition = "(e.endTime - INTERVAL " . $_SESSION['myoffset'] . " HOUR)";
         //}
                
-        $offset = $moreevents * MAX_NRO_EVENTS;
+        $max_events = MAX_NRO_EVENTS;
+        $offset = $moreevents * $max_events;
                 
         $sql = 
         "SELECT SQL_CALC_FOUND_ROWS *, e.private, ep.Events_eventID, l.name as location, l.position as pos, e.startTime, e.endTime, p.playerid, p.name,
@@ -55,7 +56,7 @@
         inner join team t on t.teamID = pt.Team_teamID
         where t.teamID = '" . $teamid  . "' and e.Team_teamID = t.teamID
         and (e.endTime - INTERVAL " . $_SESSION['myoffset'] . " HOUR) > now()
-        order by e.startTime asc, ep.Events_eventID asc, ep.areyouin desc, ep.seen desc LIMIT " . $offset . " , ". MAX_NRO_EVENTS . ";";
+        order by e.startTime asc, ep.Events_eventID asc, ep.areyouin desc, ep.seen desc LIMIT " . $offset . " , ". $max_events . ";";
         //order by e.startTime asc, ep.Events_eventID asc, ep.areyouin desc, ep.seen desc LIMIT " . MAX_NRO_EVENTS . " OFFSET " . $offset . ";";
 
 	    $result = mysql_query($sql);
