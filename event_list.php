b9ac496cc614394af06ab7d7f499da2d7b99dcab
Tuomas <tniinisto@gmail.com>
2015-10-25 17:18:47 +0200

    tune

--- event_list.php
+++ event_list.php
@@ -308,7 +308,8 @@
                 echo "<article id='more_events' class='clearfix'>";
                     echo "<div>";
                         //echo "<h3 style=\"text-align: center;\">There are more events available...</h3>";
-                        echo "<a href='#' onclick='getEvents(" . $_SESSION['more_clicks'] + 1 . ")>More events available</a>";                    
+                        $call_param = $_SESSION['more_clicks'] + 1;
+                        echo "<a href='#' onclick='getEvents(" . $call_param . ")>More events available</a>";                    
                     echo "</div>";
                 echo "</article>";
             echo "</div>";
