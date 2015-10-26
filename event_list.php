c1a88bfa905096c73cd8f5614e633d87ea20eca1
Tuomas <tniinisto@gmail.com>
2015-10-26 10:31:33 +0200

    limit on

--- event_list.php
+++ event_list.php
@@ -55,8 +55,7 @@
         inner join team t on t.teamID = pt.Team_teamID
         where t.teamID = '" . $teamid  . "' and e.Team_teamID = t.teamID
         and (e.endTime - INTERVAL " . $_SESSION['myoffset'] . " HOUR) > now()
-        order by e.startTime asc, ep.Events_eventID asc, ep.areyouin desc, ep.seen desc;";
-        //LIMIT " . $offset . " , ". MAX_NRO_EVENTS . ";";
+        order by e.startTime asc, ep.Events_eventID asc, ep.areyouin desc, ep.seen desc LIMIT " . $offset . " , ". MAX_NRO_EVENTS . ";";
         //order by e.startTime asc, ep.Events_eventID asc, ep.areyouin desc, ep.seen desc LIMIT " . MAX_NRO_EVENTS . " OFFSET " . $offset . ";";
 
 //$query = "
