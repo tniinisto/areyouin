24112d6425b4b3bae2305cea0a17c9242f4126b1
Tuomas <tniinisto@gmail.com>
2015-10-27 19:35:18 +0200

    tune

--- event_list.php
+++ event_list.php
@@ -299,7 +299,8 @@
 
             $call = $_SESSION['more_clicks'] + 1;
             echo "<div id='more_events_content'>";
-                echo "<article id='more_events' style='background-color: #6a6a6a; width: 99%;' class='clearfix'>";
+                //echo "<article id='event_article_id' style='background-color: #6a6a6a; width: 99%;' class='clearfix'>";
+                echo "<article id='event_article_id' class='clearfix'>";
                     echo "<div class='divtable'>";
                         //echo "<h3 style=\"text-align: center;\">Total " . $totalrows . "</h3>";
                         //echo "<h3 style=\"text-align: center;\">MAX_NRO_EVENTS " . MAX_NRO_EVENTS . "</h3>";
