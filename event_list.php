29d9fdf09a10505ace49006ee8fd6ff6c6b0598b
Tuomas <tniinisto@gmail.com>
2015-10-27 20:41:42 +0200

    t

--- event_list.php
+++ event_list.php
@@ -306,13 +306,13 @@
                         //echo "<h3 style=\"text-align: center;\">MAX_NRO_EVENTS " . MAX_NRO_EVENTS . "</h3>";
                         echo "<table class='atable_summary'>";
 				            echo "<tr style='cursor: pointer;'>";
-                                //echo "<th><h3 style='text-align: center;'><a href='#' style='color: #ffd800; text-decoration: underline;' onclick='getEvents(" . $call . ")'>More events available</a></h3></th>";7
-                                echo "<th style='text-align: center; color: #ffd800; text-decoration: underline;'><a href='#' style='color: #ffd800;'  onclick='getEvents(" . $call . ")'>More events available</a></th>";                
+                                echo "<th style='text-align: center; color: #ffd800; text-decoration: underline;'><a href='#' style='color: #ffd800;'  onclick='getEvents(" . $call . ")'>More events available</a></th>";        
 				            echo "</tr>";
-				            //echo "<tr style=\"cursor: pointer;\">";
+				            //echo "<tr style='cursor: pointer;'>";
                 //                echo "<th>&nbsp</th>";
 				            //echo "</tr>";
-    		            echo "</table>";            
+    		          //  echo "</table>";
+                        echo "&nbsp";            
                     echo "</div>";
                 echo "</article>";
             echo "</div>";
