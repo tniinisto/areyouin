f8e24868d4b196a6f610d4079eb032f6345f9d5e
Tuomas <tniinisto@gmail.com>
2015-10-30 21:44:32 +0200

    tune

--- event_list.php
+++ event_list.php
@@ -311,7 +311,7 @@
                         //echo "<h3 style=\"text-align: center;\">MAX_NRO_EVENTS " . MAX_NRO_EVENTS . "</h3>";
                         echo "<table class='atable_summary'>";
 				            echo "<tr style='cursor: pointer;'>";
-                                echo "<th style='padding-bottom: 10px; text-align: center; color: #ffd800; text-decoration: underline;'><a href='#' style='color: #ffd800;'  onclick='getEvents(" . $call . ")'>More events available</a></th>";        
+                                echo "<th style='padding-bottom: 10px; text-align: center; color: #ffd800; text-decoration: underline;' onclick='getEvents(" . $call . ")'><a href='#' style='color: #ffd800;'  onclick='getEvents(" . $call . ")'>More events available</a></th>";        
 				            echo "</tr>";
     		            echo "</table>";  
                         echo "<table class='lastrow2'>";
