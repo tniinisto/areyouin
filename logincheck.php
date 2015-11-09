4521f914530eb574f029b7cf7cac05d6611bdd92
Tuomas <tniinisto@gmail.com>
2015-11-09 19:05:50 +0200

    team select spinner

--- logincheck.php
+++ logincheck.php
@@ -144,14 +144,14 @@
                     echo "var spinnerTeamlogin;";
 
                     echo "var opts = {
-                        lines: 17 // The number of lines to draw
-                        , length: 5 // The length of each line
-                        , width: 16 // The line thickness
-                        , radius: 42 // The radius of the inner circle
+                        lines: 15 // The number of lines to draw
+                        , length: 2 // The length of each line
+                        , width: 4 // The line thickness
+                        , radius: 10 // The radius of the inner circle
                         , scale: 1 // Scales overall size of the spinner
                         , corners: 1 // Corner roundness (0..1)
-                        , color: '#000' // #rgb or #rrggbb or array of colors
-                        , opacity: 0.2 // Opacity of the lines
+                        , color: '#fff' // #rgb or #rrggbb or array of colors
+                        , opacity: 0.25 // Opacity of the lines
                         , rotate: 0 // The rotation offset
                         , direction: 1 // 1: clockwise, -1: counterclockwise
                         , speed: 1 // Rounds per second
@@ -159,7 +159,7 @@
                         , fps: 20 // Frames per second when using setTimeout() as a fallback for CSS
                         , zIndex: 2e9 // The z-index (defaults to 2000000000)
                         , className: 'spinner' // The CSS class to assign to the spinner
-                        , top: '50%' // Top position relative to parent
+                        , top: '10.5%' // Top position relative to parent
                         , left: '50%' // Left position relative to parent
                         , shadow: false // Whether to render a shadow
                         , hwaccel: false // Whether to use hardware acceleration
