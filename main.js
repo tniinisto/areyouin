1f295198c32ca9c476930f192eb7055a5707c382
Tuomas <tniinisto@gmail.com>
2015-12-02 14:51:52 +0200

    spinner to fixed 50/50

--- main.js
+++ main.js
@@ -995,8 +995,8 @@ function initSpinner() {
         , fps: 20 // Frames per second when using setTimeout() as a fallback for CSS
         , zIndex: 2e9 // The z-index (defaults to 2000000000)
         , className: 'spinner' // The CSS class to assign to the spinner
-        , top: '4%' // Top position relative to parent
-        , left: '90%' // Left position relative to parent
+        , top: '50%' // Top position relative to parent
+        , left: '50%' // Left position relative to parent
         , shadow: false // Whether to render a shadow
         , hwaccel: false // Whether to use hardware acceleration
         , position: 'fixed' // Element positioning
