0f0e2730a857b1b0d93de9ffce74a9c5d2da37a7
tniinisto <tniinisto@gmail.com>
2016-01-10 11:11:26 +0200

    fix google maps init

--- main.js
+++ main.js
@@ -1043,15 +1043,17 @@ function getWeather() {
 
 //Location////////////////////////////////////////////////////////////////////////////////////
 
+var map;
+
 function initializeMap() {
     var mapCanvas = document.getElementById('Location_map');
-    var map = new google.maps.Map(mapCanvas);
+    map = new google.maps.Map(mapCanvas);
 }
 
 function listenLocation() {
-    var map2 = new GMap2(document.getElementById("Location_map"));
+    //var map2 = new GMap2(document.getElementById("Location_map"));
 
-    GEvent.addListener(map2, "click", function (marker, point) {
+    GEvent.addListener(map, "click", function (marker, point) {
         var latitude = point.y;
         var longitude = point.x;
 
