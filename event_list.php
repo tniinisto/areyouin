63e63770278e62d0be9864bc81519f6d84e9bd72
Tuomas <tniinisto@gmail.com>
2016-01-29 18:09:22 +0200

    style the event colors

--- event_list.php
+++ event_list.php
@@ -131,7 +131,7 @@
 
                             //Still more than 1 player needed, yellow
                             //if($minimum - $row5['players_in'] > 1) {
-                                echo "<th id=\"id_summary" . $event_check . "\" style=\"text-align: center; color: #ffd800;\" onClick=\"showPlayers(" . $event_check . ")\">
+                                echo "<th id=\"id_summary" . $event_check . "\" style=\"text-align: center; color: #CEB425;\" onClick=\"showPlayers(" . $event_check . ")\">
                                 Event status: " . $row5['players_in'] . " / " . $row4['player_amount'] . "</th>";
                             //}
                             //else {
