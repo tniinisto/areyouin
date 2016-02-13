8a7af612208b3daa9be2fdc9d6bfd51b499db654
Tuomas <tniinisto@gmail.com>
2016-02-13 19:18:39 +0200

    what???

--- chat.php
+++ chat.php
@@ -141,7 +141,7 @@
 
                     //Latest message datetime
                     //echo "<div id='latestMsg' style='visibility: hidden;'>value: " . $_SESSION['mylastmsg'] . "</div>";
-                    echo "<div id='latestMsg'>value: " . $_SESSION['mylastmsg'] . "</div>";
+                    //echo "<div id='latestMsg'>value: " . $_SESSION['mylastmsg'] . "</div>";
 
                 echo "</div>";
 
