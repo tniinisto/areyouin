daece5b2cd32b94d22de4c4f41a4e1a8410b3cc6
tniinisto <tniinisto@gmail.com>
2016-02-15 18:57:45 +0200

    back to date comparison

--- main.js
+++ main.js
@@ -1163,8 +1163,8 @@ function checkMsgStatus() {
     //      "\nlast seen: " + document.getElementById("latestSeenMsg").textContent);
 
     //Show icon if there are newer messages and chat view is not active
-    //if((msgdatetime == seenmsgdatetime) || $("#linkchat").hasClass("current")) {
-    if((msgdatetime.localeCompare(seenmsgdatetime) == 0) || $("#linkchat").hasClass("current")) {
+    if((msgdatetime == seenmsgdatetime) || $("#linkchat").hasClass("current")) {
+    //if((msgdatetime.localeCompare(seenmsgdatetime) == 0) || $("#linkchat").hasClass("current")) {
         $("#msg_icon").addClass("noshow");
     }
     else {       
