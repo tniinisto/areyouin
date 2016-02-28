3606d50971181bb7c96d02ca666b47dd05d7b7fc
Tuomas <tniinisto@gmail.com>
2016-02-28 14:46:19 +0200

    fixed sql clause

--- updatePlayer.php
+++ updatePlayer.php
@@ -28,7 +28,7 @@
         $player_notify = 0;
 
     $sql = "UPDATE players SET mail = '" . $player_email ."', mobile = '" . $player_phone . "', notify = '" . $player_notify . "',
-            name = '" . $player_name . ", firstname = '" . $player_firstname . ", lastname = '" . $player_lastname . "
+            name = '" . $player_name . "', firstname = '" . $player_firstname . "', lastname = '" . $player_lastname . "'
             WHERE playerID = " . $_SESSION['myplayerid'] . ";";
 
 
