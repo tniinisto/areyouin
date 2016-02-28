cd45efc236dff309620e27c8dc7cc61e8c6280c9
Tuomas <tniinisto@gmail.com>
2016-02-28 13:34:22 +0200

    map zoom to 6

--- main.js
+++ main.js
@@ -1073,13 +1073,13 @@ var nlat = 0, nlon = 0;
     if (nlat != 0) {
         var mapOptions = {
             center: new google.maps.LatLng(nlat, nlon),
-            zoom: 5,
+            zoom: 6,
             mapTypeId: google.maps.MapTypeId.ROADMAP
         }
     } else {
         var mapOptions = {
             center: new google.maps.LatLng(60,387, 23,134),
-            zoom: 5,
+            zoom: 6,
             mapTypeId: google.maps.MapTypeId.ROADMAP
         }
     }
