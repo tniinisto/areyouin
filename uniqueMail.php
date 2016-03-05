cd21431b1ebbf7c55702426709632e32fd092f38
Tuomas <tniinisto@gmail.com>
2016-03-05 19:15:34 +0200

    debug

--- uniqueMail.php
+++ uniqueMail.php
@@ -26,7 +26,7 @@ try {
 
 	$dbh = null;
 
-    if($_SESSION['ChromeLog']) { ChromePhp::log("mailcount: " . $mailcount); }
+    if($_SESSION['ChromeLog']) { ChromePhp::log("mailcount: " . $mailcount[0]); }
 
 	echo '{"items":'. json_encode($mailcount) .'}'; 
 }
