eef2a2fa12e0e66ce6e92ca6c850d7cbd1798bc9
Tuomas <tniinisto@gmail.com>
2016-03-05 19:51:52 +0200

    fixing

--- uniqueMail.php
+++ uniqueMail.php
@@ -7,9 +7,18 @@
         require_once 'ChromePhp.php';
         ChromePhp::log('uniqueMail.php, start');
     }
-        
 
     $mail=$_GET["mail"];
+    
+    $con = mysql_connect('eu-cdbr-azure-north-a.cloudapp.net', 'bd3d44ed2e1c4a', '8ffac735');
+	
+    if (!$con)
+    {
+	    die('Could not connect: ' . mysql_error());
+	}
+
+    mysql_select_db("areyouin", $con);    
+
 
     $sql = "SELECT count(mail) as mailcount from players WHERE mail like '" . $mail . "'";
 	$result = mysql_query($sql);
@@ -17,25 +26,26 @@
     echo "Count: " . $row['mailcount'];
 
 
-//try {
-//    //PDO means "PHP Data Objects"
-//    //dbh meand "Database handle"
-//    //STH means "Statement Handle"
 
-//	$dbh = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);	
+    //try {
+    //    //PDO means "PHP Data Objects"
+    //    //dbh meand "Database handle"
+    //    //STH means "Statement Handle"
 
-//	$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-//	$stmt = $dbh->query($sql);  
-//	$mailcount = $stmt->fetchAll(PDO::FETCH_OBJ);
+    //	$dbh = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);	
 
-//	$dbh = null;
+    //	$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    //	$stmt = $dbh->query($sql);  
+    //	$mailcount = $stmt->fetchAll(PDO::FETCH_OBJ);
 
-//    //if($_SESSION['ChromeLog']) { ChromePhp::log("mailcount: " . $mailcount[0]); }
+    //	$dbh = null;
 
-//	echo '{"items":'. json_encode($mailcount) .'}'; 
-//}
-//catch(PDOException $e) {
-//	echo '{"error":{"text":'. $e->getMessage() .'}}'; 
-//}
+    //    //if($_SESSION['ChromeLog']) { ChromePhp::log("mailcount: " . $mailcount[0]); }
+
+    //	echo '{"items":'. json_encode($mailcount) .'}'; 
+    //}
+    //catch(PDOException $e) {
+    //	echo '{"error":{"text":'. $e->getMessage() .'}}'; 
+    //}
 
 ?>
