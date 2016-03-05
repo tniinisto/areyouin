f62616ae154019c208a2c2438b87b7618968e1d2
Tuomas <tniinisto@gmail.com>
2016-03-05 09:40:47 +0200

    tune

--- player_profile.php
+++ player_profile.php
@@ -113,19 +113,19 @@
                                 echo "</p>";
                                 
                                 //Fullname
-                                echo "<p style='margin: 0px; padding-top: 5px;'>";
+                                echo "<p style='margin: 0px; padding-top: 1px;'>";
                                 echo "<label for='player_firstname' style='display: inline-block; width: 60px; text-align: right; color:black;'>Firstname:&nbsp</label>";                    
                                 echo "<input type='text' id='dialog_player_firstname' name='player_firstname' value='" . $player->firstname ."' required style='margin-bottom: 15px; width: 180px;'></input>";
                                 echo "</p>";
 
                                 //Lastname
-                                echo "<p style='margin: 0px; padding-top: 2px;'>";
+                                echo "<p style='margin: 0px; padding-top: 1px;'>";
                                 echo "<label for='player_lastname' style='display: inline-block; width: 60px; text-align: right; color:black;'>Lastname:&nbsp</label>";                    
                                 echo "<input type='text' id='dialog_player_lastname' name='player_lastname' value='" . $player->lastname ."' required style='margin-bottom: 15px; width: 180px;'></input>";
                                 echo "</p>";
 
                                 //Phone
-                                echo "<p style='margin: 0px; padding-top: 2px; padding-bottom: 5px;'>";
+                                echo "<p style='margin: 0px; padding-top: 1px; padding-bottom: 5px;'>";
                                 echo "<label for='player_phone' style='display: inline-block; width: 60px; text-align: right; color:black;'>Phone:&nbsp</label>";
                                 echo "<input type='text' id='dialog_player_phone' name='player_phone' value='" . $player->phone ."' required style='margin-bottom: 15px; width: 180px;'></input>";
                                 echo "</p>";
