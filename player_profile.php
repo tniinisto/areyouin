e1136c94b7435c7e9eff5bea0bbacc89d8cac7c8
Tuomas <tniinisto@gmail.com>
2016-03-06 10:11:27 +0200

    get

--- player_profile.php
+++ player_profile.php
@@ -90,7 +90,7 @@
 		                    <a id="closer" href="#close" title="Close" class="close">X</a>
                     <?php
 
-                            echo "<form id='player_edit' name='player_edit' method='post' target='frame_player' onsubmit='UpdatePlayer();'>";
+                            echo "<form id='player_edit' name='player_edit' method='get' target='frame_player' onsubmit='UpdatePlayer();'>";
 
                                 //echo "<p style='margin: 5px;'>";
                                 //echo "<label style='display: block; text-align: center; weight: bold; width: 110%; font-size: 125%;'>Edit your information</label>";
