00be1c09f34bc9f394417b55e7a80c604b22f97b
tniinisto <tniinisto@gmail.com>
2016-03-08 18:03:27 +0200

    tune

--- players_insert.php
+++ players_insert.php
@@ -310,7 +310,7 @@
                                 echo "</p>";
 
                                 echo "<p style='margin: 0px; padding-top: 10px;'>";
-                                echo "<label for='player_name". $index . "' style='display inline-block; width: 60px; text-align: right;'>User ID:&nbsp</label>";                    
+                                echo "<label for='player_name". $index . "' style='display: inline-block; width: 60px; text-align: right;'>User ID:&nbsp</label>";                    
                                 echo "<input type='text' id='dialog_player_name". $index . "' name='player_name". $index . "' value='". $player->name . "'' required style='margin-bottom: 15px; background: grey; width: 190px;' readonly></input>";
                                 echo "</p>";
 
