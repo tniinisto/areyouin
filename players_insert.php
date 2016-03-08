f21b068ea2d4fc4c3f112ea381e8f22ab0c5d292
tniinisto <tniinisto@gmail.com>
2016-03-08 18:08:08 +0200

    style class to use

--- players_insert.php
+++ players_insert.php
@@ -325,7 +325,7 @@
                                 echo "</p>";
 
 
-                                echo "<h5 id='dialog_player_notify". $index . "'>Team Admin: </h5>";
+                                echo "<h5 id='dialog_player_notify". $index . "' class='dialog_player_notify'>Team Admin: </h5>";
                                     if( $player->teamAdmin == 1) {
                                         echo "<div class='onoffswitch notifyswitch' style='display: inline-block;'>";
 						                    echo "<input type='checkbox' name='adminswitch' class=\"onoffswitch-checkbox\" id='dialog_admin_switch". $index . "' checked>";					            
