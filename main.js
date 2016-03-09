60da53d764d7f188e81d66c7cb188400962d9c28
tniinisto <tniinisto@gmail.com>
2016-03-09 23:51:58 +0200

    update list on

--- main.js
+++ main.js
@@ -1296,7 +1296,7 @@ function updateAdminStatus(playerID, admin_checkbox) {
 
         xmlhttp.onreadystatechange = function () {
             if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-                //updateUserlist();
+                updateUserlist();
                 window.location.replace('#close');
             }
         }
