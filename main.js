7b3ae420625b3409d0c3e4714530eef888d74580
Tuomas <tniinisto@gmail.com>
2016-03-12 15:34:17 +0200

    debug

--- main.js
+++ main.js
@@ -1379,7 +1379,8 @@ function waitForEventUpdate(){
 
             //Get comments only if php not timed out...
             if (json['timeout'] == 0) {
-                alert("success timeout false: " + json['timeout']);
+                //alert("success timeout false: " + json['timeout']);
+                alert("json timestamp: "+ json['timestamp']);
                 //setTimeout('getEventsAsync()', 100);
                 eventparameter = json['timestamp'];
                 getEventsAsync();
