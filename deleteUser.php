150b8ed171713dd90e1a7a64eb7bea3c0c6b690b
Tuomas <tniinisto@gmail.com>
2016-04-03 10:13:29 +0300

    player delete, handling case if player in multiple teams

--- deleteUser.php
+++ deleteUser.php
@@ -9,19 +9,46 @@
         ChromePhp::log('deleteUser.php start');
     }
 
+
+    //$playerId=$_GET['playerID'];
+
   	$dbh = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);	
 	$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
           
-    try {    
-        //$sql1 = "DELETE FROM eventplayer WHERE Events_eventID = :eventId";
-        //$stmt1 = $dbh->prepare($sql1);
-        //$stmt1->bindParam(':eventId', $_POST['delete_eventid'], PDO::PARAM_INT);   
-        //$stmt1->execute();
+    try {
 
-        //$sql2 = "DELETE FROM events WHERE eventID = :eventId";
-        //$stmt2 = $dbh->prepare($sql2);
-        //$stmt2->bindParam(':eventId', $_POST['delete_eventid'], PDO::PARAM_INT);   
-        //$stmt2->execute();
+        //Update the removed player teamID to nullTeam, teamId = 0 for current team, player can be in multiple teams
+        $sql = "UPDATE playerteam SET Team_teamID = '0' WHERE Players_playerID =  :playerID AND Team_teamid = :teamID";
+
+        if($_SESSION['ChromeLog']) { ChromePhp::log('delete player: ' . $sql); }
+        
+        $stmt = $dbh->prepare($sql);
+        $stmt->bindParam(':playerID', $_GET['playerID'], PDO::PARAM_INT);
+        $stmt->bindParam(':teamID', $_SESSION['myteamid'], PDO::PARAM_INT);
+        $stmt->execute();
+
+
+        //Count TeamMemberCount for the player
+        $sql2 = "select count(players_playerid) as TeamMemberCount from playerteam where players_playerid = :playerID";
+
+        $stmt2 = $dbh->prepare($sql2);
+        $stmt2->bindParam(':playerID', $_GET['playerID'], PDO::PARAM_INT);
+        $stmt2->execute();
+        $teamCount = 0;
+        while($row = $stmt2->fetch()) {
+            //print_r($row);
+            $teamCount = $row['TeamMemberCount'];
+        }
+
+        //If player only in one team -> Clear email value, so same email can be used when making a new player for the same mail in future
+        if($teamCount < 2) {
+            $sql1 = "UPDATE players SET mail = '' WHERE playerID =  :playerID";
+            if($_SESSION['ChromeLog']) { ChromePhp::log('delete player: ' . $sql1); }
+        
+            $stmt1 = $dbh->prepare($sql1);
+            $stmt1->bindParam(':playerId', $_GET['playerID'], PDO::PARAM_INT);   
+            $stmt1->execute();
+        }
         
         if($_SESSION['ChromeLog']) { ChromePhp::log('deleteUser.php: ', $sql1); }
 
