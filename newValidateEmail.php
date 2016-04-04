0ff4b482b8ed390aaaf075d52f47c327a9a75fa8
Tuomas <tniinisto@gmail.com>
2016-04-04 12:18:18 +0300

    mailcount

--- newValidateEmail.php
+++ newValidateEmail.php
@@ -20,7 +20,10 @@
         //Validate the email, does it already exist, is user already in the team. If already in another team, show name and ask if this should be insterted for the team
 
         //Check mail address, return count of matching addresses
-        $sql = "SELECT count(mail) as mailcount from players where mail like :mail";
+        $sql = "SELECT mail, count(mail) as mailcount, t.teamName, t.teamID from players p
+                inner join playerteam pt on p.playerID = pt.Players_playerID
+                inner join team t on t.teamID = pt.Team_teamID
+                where p.mail like :mail";
 
         if($_SESSION['ChromeLog']) { ChromePhp::log('newValidateEmail mailcount: ' . $sql); }
         
@@ -28,14 +31,22 @@
         $stmt->bindParam(':mail', $_GET['mail'], PDO::PARAM_STR);
         $stmt->execute();
 
+        $row;
         $mailCount = 0;
         while($row = $stmt->fetch()) {
             //print_r($row);
             $mailCount = $row['mailcount'];
         }
 
-        //Return mailCount
-        echo $mailCount;
+        
+        //Check if mail address already in use
+        if($mailCount > 0) {
+            //Return mailCount and team
+            echo $mailCount . "," . $row['teamID'] ;
+        } else {
+            //Return mailCount
+            echo $mailCount;
+        }
 
         $dbh = null;
     }
