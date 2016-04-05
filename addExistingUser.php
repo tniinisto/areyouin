45539db56756788b6093775028c852b9c8217b87
Tuomas <tniinisto@gmail.com>
2016-04-05 20:26:45 +0300

    tune sql

--- addExistingUser.php
+++ addExistingUser.php
@@ -16,7 +16,7 @@
     try {
 
         //Add existing player to the team//////////////////////////////////////////////////////////
-        $sql1 = "INSERT INTO playerteam (Players_playerID, Team_teamID, teamAdmin) VALUES ( :playerid," . $_SESSION['myteamid'] . ", 0)";
+        $sql1 = "INSERT INTO playerteam (Players_playerID, Team_teamID, teamAdmin) VALUES ( :playerid ," . $_SESSION['myteamid'] . ", 0)";
 
         if($_SESSION['ChromeLog']) { ChromePhp::log('Add player for playerteam: ' . $sql1); }
 
