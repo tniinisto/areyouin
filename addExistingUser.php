196fb30c7e0667635f0fd36e8c7ab50935f76db1
Tuomas <tniinisto@gmail.com>
2016-04-05 20:17:25 +0300

    adding existing player

--- addExistingUser.php
+++ addExistingUser.php
@@ -15,25 +15,17 @@
           
     try {
 
-        //Get playerID for mail address////////////////////////////////////////////////////////////
-        $sql = "SELECT playerID from players WHERE mail like ' :mail '";
-        
-        $stmt = $dbh->prepare($sql);
-        $stmt->bindParam(':mail', $_GET['mail'], PDO::PARAM_STR);        
-        $stmt->execute();                        
-
-        $row = $stmt->fetch();
-        $playerid = $row['playerID'];
-
         //Add existing player to the team//////////////////////////////////////////////////////////
-        $sql1 = "INSERT INTO playerteam (Players_playerID, Team_teamID, teamAdmin) VALUES (" . $playerid . "," . $_SESSION['myteamid'] . ", 0)";
+        $sql1 = "INSERT INTO playerteam (Players_playerID, Team_teamID, teamAdmin) VALUES ( :playerid," . $_SESSION['myteamid'] . ", 0)";
 
         if($_SESSION['ChromeLog']) { ChromePhp::log('Add player for playerteam: ' . $sql1); }
-        
-        $stmt1 = $dbh->prepare($sql1);      
+
+        $stmt1 = $dbh->prepare($sql1);    
+        $stmt1->bindParam(':playerID', $_GET['playerid'], PDO::PARAM_INT);
+  
         $result1 = $stmt1->execute();                        
 
-        echo 
+        echo $result1;
 
         $dbh = null;
 
