54d45019dd02fa91cc373f6468a9d35ee5ccdf59
Tuomas <tniinisto@gmail.com>
2016-04-06 13:48:09 +0300

    debug

--- main.js
+++ main.js
@@ -1495,6 +1495,7 @@ function newValidateEmail(mail, teamid) {
     xmlhttp.onreadystatechange = function () {
         if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
 
+            alert(xmlhttp.responseText);
 
             //mails
             var t = xmlhttp.responseText.split(/:/);
