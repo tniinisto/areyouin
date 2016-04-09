f92f914565825b9c74cd8c9365b5fd14d946e4c5
Tuomas <tniinisto@gmail.com>
2016-04-09 07:43:28 +0300

    the setting also need to bet set for PDO :) doh!

--- setTeam.php
+++ setTeam.php
@@ -17,21 +17,31 @@
     $teamid=$_POST['teamselect'];
     $_SESSION['myteamid'] = $teamid;
 
-    //Check & set team admin status for user
-    $sql1 = "SELECT teamAdmin FROM playerteam where Team_teamID = :teamid AND Players_playerID = :player";
 
-    if($_SESSION['ChromeLog']) { ChromePhp::log('set admin status: ' . $sql1); }
+  	$dbh = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);	
+	$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    try {
+
+        //Check & set team admin status for user
+        $sql1 = "SELECT teamAdmin FROM playerteam where Team_teamID = :teamid AND Players_playerID = :player";
+
+        if($_SESSION['ChromeLog']) { ChromePhp::log('set admin status: ' . $sql1); }
         
-    $stmt1 = $dbh->prepare($sql1);
-    $stmt1->bindParam(':player', $_SESSION['myplayerid'], PDO::PARAM_INT);
-    $stmt1->bindParam(':teamid', $_SESSION['myteamid'], PDO::PARAM_INT);
-    $stmt1->execute();
-    $row1 = $stmt1->fetch();
+        $stmt1 = $dbh->prepare($sql1);
+        $stmt1->bindParam(':player', $_SESSION['myplayerid'], PDO::PARAM_INT);
+        $stmt1->bindParam(':teamid', $_SESSION['myteamid'], PDO::PARAM_INT);
+        $stmt1->execute();
+        $row1 = $stmt1->fetch();
 
-    //Set chosen teams admin status  to session
-    $_SESSION['myAdmin'] = $row1['teamAdmin'];
+        //Set chosen teams admin status  to session
+        $_SESSION['myAdmin'] = $row1['teamAdmin'];
 
 
-    header('Location:login_success.php');
+        header('Location:login_success.php');
+    }
+    catch(PDOException $e) {
+	    echo '{"error":{"text":'. $e->getMessage() .'}}'; 
+    }
   
 ?>
