1bcde3e6c174f50189c7f04f860f1896b4ea42f9
Tuomas <tniinisto@gmail.com>
2016-04-09 07:38:35 +0300

    db config was missing

--- setTeam.php
+++ setTeam.php
@@ -1,30 +1,37 @@
 <?php
-        // No cache
-        header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
-        header("Cache-Control: no-cache");
-        header("Pragma: no-cache");
+    include( $_SERVER['DOCUMENT_ROOT'] . '/config/config.php' );
 
-        session_start();
+    session_start();
 
-        //Set team to session
-        $teamid=$_POST['teamselect'];
-        $_SESSION['myteamid'] = $teamid;
+    if($_SESSION['ChromeLog']) {
+        require_once 'ChromePhp.php';
+        ChromePhp::log('setTeam.php start');
+    }
 
-        //Check & set team admin status for user
-        $sql1 = "SELECT teamAdmin FROM playerteam where Team_teamID = :teamid AND Players_playerID = :player";
+    // No cache
+    header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+    header("Cache-Control: no-cache");
+    header("Pragma: no-cache");
 
-        if($_SESSION['ChromeLog']) { ChromePhp::log('set admin status: ' . $sql1); }
+    //Set team to session
+    $teamid=$_POST['teamselect'];
+    $_SESSION['myteamid'] = $teamid;
+
+    //Check & set team admin status for user
+    $sql1 = "SELECT teamAdmin FROM playerteam where Team_teamID = :teamid AND Players_playerID = :player";
+
+    if($_SESSION['ChromeLog']) { ChromePhp::log('set admin status: ' . $sql1); }
         
-        $stmt1 = $dbh->prepare($sql1);
-        $stmt1->bindParam(':player', $_SESSION['myplayerid'], PDO::PARAM_INT);
-        $stmt1->bindParam(':teamid', $_SESSION['myteamid'], PDO::PARAM_INT);
-        $stmt1->execute();
-        $row1 = $stmt1->fetch();
+    $stmt1 = $dbh->prepare($sql1);
+    $stmt1->bindParam(':player', $_SESSION['myplayerid'], PDO::PARAM_INT);
+    $stmt1->bindParam(':teamid', $_SESSION['myteamid'], PDO::PARAM_INT);
+    $stmt1->execute();
+    $row1 = $stmt1->fetch();
 
-        //Set chosen teams admin status  to session
-        $_SESSION['myAdmin'] = $row1['teamAdmin'];
+    //Set chosen teams admin status  to session
+    $_SESSION['myAdmin'] = $row1['teamAdmin'];
 
 
-        header('Location:login_success.php');
+    header('Location:login_success.php');
   
 ?>
