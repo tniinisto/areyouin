9f4fe1c4d12d9980736759e3d492a723d0882f4d
Tuomas <tniinisto@gmail.com>
2016-04-24 12:14:53 +0300

    test

--- players_insert.php
+++ players_insert.php
@@ -237,7 +237,7 @@
                         echo "<div id='locations_list' class='scrollit2'>";
                             echo "<h2>Test location header...</h2>";
 
-                         //   echo "<table border='0' id='locations_table' class='usertable'";
+                            echo "<table border='0' id='locations_table' class='usertable'";
                         //
                         //        mysql_data_seek($result, 0);
                         //        $index_locations = 1000;
@@ -298,7 +298,7 @@
                         //                $index_locations++;
                         //        }
 
-                        //    echo "</table>";
+                            echo "</table>";
 
                         echo "</div>"; //Scrollit, end of locations list//////////////////////////////
 
