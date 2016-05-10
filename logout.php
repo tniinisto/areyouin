53528b3a1af8964c4584423500b7a080999c5304
Tuomas <tniinisto@gmail.com>
2016-05-10 13:03:08 +0300

    clear php session variables on logout

--- logout.php
+++ logout.php
@@ -10,6 +10,9 @@
  	
     session_unset(); 	
     session_destroy();
+    session_write_close();
+    setcookie(session_name(),'',0,'/');
+    session_regenerate_id(true);
 
     //ChromePhp::log("logout.php end, logged_in:", $_SESSION['logged_in']);
 
