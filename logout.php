e9dc2af9310cf390e17a9f39005eff4d869d3a0b
Tuomas <tniinisto@gmail.com>
2016-05-29 20:33:07 +0300

    php session destroy on logout, set null array first, then unset and finaly destroy

--- logout.php
+++ logout.php
@@ -8,11 +8,11 @@
 
     //ChromePhp::log("logout.php start, logged_in:", $_SESSION['logged_in']);
 
-    setcookie(session_name(), '', 100);
+    //setcookie(session_name(), '', 100);
+    $_SESSION = array();
     session_unset();
     session_destroy();
-    $_SESSION = array();
-
+    
     //ChromePhp::log("logout.php end, logged_in:", $_SESSION['logged_in']);
 
     //header("location:default.html");
