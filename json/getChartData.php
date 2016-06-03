2e47d0697785cc85929643ceb2d497723215b7a3
Tuomas <tniinisto@gmail.com>
2016-06-03 14:03:06 +0300

    rewrite the chart date sql clause

--- json/getChartData.php
+++ json/getChartData.php
@@ -11,20 +11,30 @@
     //                    GROUP BY YEAR(events.startTime), MONTH(events.startTime);";
 
     
+    //$sql_team_events = 
+    //"SELECT CONCAT(year, '/' ,month) as month, participated, games
+    //FROM
+    //(SELECT eventID, YEAR(e1.startTime) as year, MONTH(e1.startTime) as month, count(e1.eventID) as games
+    //FROM `areyouin`.`events` e1
+    //where Team_teamID = '" . $teamid . "'
+    //GROUP BY YEAR(e1.startTime), MONTH(e1.startTime)) t1
+    //left join
+    //(SELECT eventID, YEAR(e2.startTime) as year2, MONTH(e2.startTime) as month2, count(ep.eventPlayerID) as participated
+    //FROM eventplayer ep, events e2
+    //where ep.Players_playerID = '" . $playerid. "' and ep.areyouin = 1 and e2.eventID = ep.Events_eventID
+    //GROUP BY YEAR(e2.startTime), MONTH(e2.startTime)) t2
+    //on t1.eventID = t2.eventID;";
+    //
+
     $sql_team_events = 
-    "SELECT CONCAT(year, '/' ,month) as month, participated, games
-    FROM
-    (SELECT eventID, YEAR(e1.startTime) as year, MONTH(e1.startTime) as month, count(e1.eventID) as games
+    "select CONCAT(YEAR(e.startTime), '/', MONTH(e.endTime)) as time, count(ep.eventPlayerID) as participated, count(t2.eventID) as games
+    from areyouin.events e, areyouin.eventplayer ep,
+    (SELECT e1.eventID, count(e1.eventID) as games
     FROM `areyouin`.`events` e1
-    where Team_teamID = '" . $teamid . "'
-    GROUP BY YEAR(e1.startTime), MONTH(e1.startTime)) t1
-    left join
-    (SELECT eventID, YEAR(e2.startTime) as year2, MONTH(e2.startTime) as month2, count(ep.eventPlayerID) as participated
-    FROM eventplayer ep, events e2
-    where ep.Players_playerID = '" . $playerid. "' and ep.areyouin = 1 and e2.eventID = ep.Events_eventID
-    GROUP BY YEAR(e2.startTime), MONTH(e2.startTime)) t2
-    on t1.eventID = t2.eventID;";
-    
+    where e1.Team_teamID = '" . $teamid . "') t2
+    where e.eventID = ep.Events_eventID and e.Team_teamID = '" . $teamid . "' and ep.Players_playerID = '" . $playerid. "' and ep.areyouin = 1
+    group by time;";
+
     try {
         //PDO means "PHP Data Objects"
         //dbh meand "Database handle"
