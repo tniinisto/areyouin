623dd76334824ea4b5076d52963ae3737a521a0a
Tuomas <tniinisto@gmail.com>
2016-06-03 17:32:16 +0300

    gogogo, chart sql solved...maybe :)

--- json/getChartData.php
+++ json/getChartData.php
@@ -27,13 +27,18 @@
     //
 
     $sql_team_events = 
-    "select CONCAT(YEAR(e.startTime), '/', MONTH(e.endTime)) as month, count(ep.eventPlayerID) as participated, count(t2.eventID) as games
-    from areyouin.events e, areyouin.eventplayer ep,
-    (SELECT e1.eventID, count(e1.eventID) as games
-    FROM `areyouin`.`events` e1
-    where e1.Team_teamID = '" . $teamid . "') t2
-    where e.eventID = ep.Events_eventID and e.Team_teamID = '" . $teamid . "' and ep.Players_playerID = '" . $playerid. "' and ep.areyouin = 1
-    group by month;";
+       "SELECT CONCAT(year, '/' ,month) as month1, IFNULL(participated, 0), games
+        FROM
+        (SELECT eventID, YEAR(e1.startTime) as year, MONTH(e1.startTime) as month, count(e1.eventID) as games
+        FROM `areyouin`.`events` e1
+        where Team_teamID = '" . $teamid . "'
+        GROUP BY YEAR(e1.startTime), MONTH(e1.startTime)) t1
+        left join
+        (SELECT eventID, YEAR(e2.startTime) as year2, MONTH(e2.startTime) as month2, count(ep.eventPlayerID) as participated
+        FROM eventplayer ep, events e2
+        where ep.Players_playerID = '" . $playerid. "' and ep.areyouin = 1 and e2.eventID = ep.Events_eventID and e2.Team_teamID = '" . $teamid . "'
+        GROUP BY YEAR(e2.startTime), MONTH(e2.startTime)) t2
+        on t1.eventID = t2.eventID;";
 
     try {
         //PDO means "PHP Data Objects"
