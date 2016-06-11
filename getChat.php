ca3555341a01e4856a6180decdac71431e8fea90
tniinisto <tniinisto@gmail.com>
2016-06-11 08:21:12 +0300

    tune comet

--- getChat.php
+++ getChat.php
@@ -19,6 +19,7 @@
     define('MESSAGE_TIMEOUT_SECONDS_BUFFER', 5);
         
     $teamid=$_SESSION['myteamid'];
+    $playerid = 0;
 
     // Close the session prematurely to avoid usleep() from locking other requests
     session_write_close();
