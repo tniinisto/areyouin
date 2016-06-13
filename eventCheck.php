d349134980d907ef96ca57ad072c4a76dcbe0ba4
Tuomas <tniinisto@gmail.com>
2016-06-13 18:52:17 +0300

    test off

--- eventCheck.php
+++ eventCheck.php
@@ -112,8 +112,8 @@
     $response['timestamp'] = $currentmodif;
     $response['timeout'] = $timeout;
     $response['playerid'] = $playeredited;
-    $response['param'] = $param_time;
-    $response['db'] = $db_time;
+    //$response['param'] = $param_time;
+    //$response['db'] = $db_time;
 
     echo json_encode($response);
 
