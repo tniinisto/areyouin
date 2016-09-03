ae363caeb79c44ff6f0339c513607cc7848c4c48
Tuomas <tniinisto@gmail.com>
2016-09-03 22:01:53 +0300

    register layout

--- register/js/jquery.main.js
+++ register/js/jquery.main.js
@@ -100,6 +100,10 @@ $(function(){
         $('#first_step').slideDown();  
         $('#second_step').slideUp();
 
+        //progress bar
+        $('#progress_text').html('0% Complete');
+        $('#progress').css('width','0px');
+
     });
 
     $('#submit_second').click(function(){
@@ -133,11 +137,49 @@ $(function(){
 
     });
 
+    $('#submit_prev3').click(function(){
+        //remove classes
+        $('#second_step input').removeClass('error').removeClass('valid');
+
+        // var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;  
+        // var fields = $('#second_step input[type=text]');
+        // var error = 0;
+        // fields.each(function(){
+        //     var value = $(this).val();
+        //     if( value.length<1 || value==field_values[$(this).attr('id')] || ( $(this).attr('id')=='email' && !emailPattern.test(value) ) ) {
+        //         $(this).addClass('error');
+        //         $(this).effect("shake", { times:3 }, 50);
+                
+        //         error++;
+        //     } else {
+        //         $(this).addClass('valid');
+        //     }
+        // });
+
+        // if(!error) {
+        //         //update progress bar
+        //         $('#progress_text').html('66% Complete');
+        //         $('#progress').css('width','226px');
+                
+        //         //slide steps
+        //         $('#second_step').slideDown();
+        //         $('#first_step').slideUp();     
+        // } else return false;
+
+        //slide steps
+        $('#second_step').slideDown();  
+        $('#third_step').slideUp();
+
+        //progress bar
+        $('#progress_text').html('33% Complete');
+        $('#progress').css('width','113px');
+
+    });
 
     $('#submit_third').click(function(){
         //update progress bar
         $('#progress_text').html('100% Complete');
-        $('#progress').css('width','339px');
+        $('#progress').css('width','250px');
 
         //prepare the fourth step
         var fields = new Array(
