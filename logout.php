01972f84abc4c7ec0180bd816a506895d2030aa2
Tuomas <tniinisto@gmail.com>
2016-09-16 18:38:31 +0300

    delete php session cookie

--- logout.php
+++ logout.php
@@ -25,6 +25,15 @@
 
     //setcookie(session_name(), '', 100);
     $_SESSION = array();
+
+    if (ini_get("session.use_cookies")) {
+        $params = session_get_cookie_params();
+        setcookie(session_name(), '', time() - 42000,
+            $params["path"], $params["domain"],
+            $params["secure"], $params["httponly"]
+        );
+    }
+
     session_unset();
     session_destroy();
     
