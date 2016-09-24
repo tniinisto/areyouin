3c28af8b56efcad7c049ca413314a4aaf5309b6a
Tuomas <tniinisto@gmail.com>
2016-09-24 16:26:03 +0300

    timezone function to main.js

--- register/js/jquery.main.js
+++ register/js/jquery.main.js
@@ -129,7 +129,7 @@ $(function(){
 
         if(!error) {
                 getTimezones();
-                
+
                 //update progress bar
                 $('#progress_text').html('66% Complete');
                 $('#progress').css('width','160px');
@@ -206,26 +206,4 @@ $(function(){
 
 });
 
-function getTimezones() {
-	if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
-		xmlhttp = new XMLHttpRequest();
-	}
-	else {// code for IE6, IE5
-		xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-	}
 
-	xmlhttp.onreadystatechange = function () {
-		if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-			document.getElementById("timezones").innerHTML = xmlhttp.responseText;
-		}
-	}
-
-	//alert("GET ge7ts called.");
-	//var variables = "p=" + playerID + "&t=" + teamID;
-	//var variables = "p=1&t=1";
-	//alert(variables);
-	//xmlhttp.open("GET", "logininfo.php?" + variables, false);
-	//alert("jou");
-    xmlhttp.open("GET", "timezones.php", false);
-	xmlhttp.send();
-}
--- register/js/main.js
+++ register/js/main.js
@@ -7,3 +7,27 @@ function previousStep () {
     $('#first_step').slideUp();  
     $('#second_step').slideDown();
 }
+
+function getTimezones() {
+	if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
+		xmlhttp = new XMLHttpRequest();
+	}
+	else {// code for IE6, IE5
+		xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+	}
+
+	xmlhttp.onreadystatechange = function () {
+		if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+			document.getElementById("timezones").innerHTML = xmlhttp.responseText;
+		}
+	}
+
+	//alert("GET ge7ts called.");
+	//var variables = "p=" + playerID + "&t=" + teamID;
+	//var variables = "p=1&t=1";
+	//alert(variables);
+	//xmlhttp.open("GET", "logininfo.php?" + variables, false);
+	//alert("jou");
+    xmlhttp.open("GET", "timezones.php", false);
+	xmlhttp.send();
+}
