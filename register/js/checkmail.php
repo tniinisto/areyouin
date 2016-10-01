2542ab57a86532ec43066f575ff37569344d24f3
Tuomas <tniinisto@gmail.com>
2016-10-01 14:36:01 +0300

    standard mysql to use

--- register/js/checkmail.php
+++ register/js/checkmail.php
@@ -5,32 +5,46 @@
     //Ajax url parameter
     $mail=$_GET["mail"];
 
+    //Sql connection
+    $con = mysql_connect($dbhost, $dbuser, $dbpass);
+    if (!$con)
+    {
+        die('Could not connect here: ' . mysql_error());
+    }
+    mysql_select_db("areyouin", $con);
+
     $sql = "select count(playerid) as count, firstname, lastname, name
             from players
             where mail = '" . $mail . "';";
+           
+    $result = mysql_query($sql);
+    $row = mysql_fetch_array($result);
 
-    try {
-        //PDO means "PHP Data Objects"
-        //dbh meand "Database handle"
-        //STH means "Statement Handle"
+    echo "Amount is: " + $row['count'];
 
-        $dbh = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);	
 
-        $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $stmt = $dbh->query($sql);  
-        $playerinfo = $stmt->fetchAll(PDO::FETCH_OBJ);
+    // try {
+    //     //PDO means "PHP Data Objects"
+    //     //dbh meand "Database handle"
+    //     //STH means "Statement Handle"
 
-        // foreach($playerinfo as $row) {
-        //     echo "playercount is: " + $row['count'];
-        // }
+    //     $dbh = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);	
 
-        echo "moro";
+    //     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    //     $stmt = $dbh->query($sql);  
+    //     $playerinfo = $stmt->fetchAll(PDO::FETCH_OBJ);
 
-        $dbh = null;
-        //echo '{"items":'. json_encode($playerinfo) .'}'; 
-    }
-    catch(PDOException $e) {
-        echo '{"error":{"text":'. $e->getMessage() .'}}'; 
-    }    
+    //     // foreach($playerinfo as $row) {
+    //     //     echo "playercount is: " + $row['count'];
+    //     // }
+
+    //     echo "moro";
+
+    //     $dbh = null;
+    //     //echo '{"items":'. json_encode($playerinfo) .'}'; 
+    // }
+    // catch(PDOException $e) {
+    //     echo '{"error":{"text":'. $e->getMessage() .'}}'; 
+    // }    
 
 ?>
