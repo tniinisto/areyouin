d4ff23267307f0866f6b06f8c3990f61acee8661
tniinisto <tniinisto@gmail.com>
2016-10-07 16:00:05 +0300

    stop spinner if !valid

--- register/js/jquery.main.js
+++ register/js/jquery.main.js
@@ -78,6 +78,7 @@ $(function(){
         }               
         else {
             document.getElementById('submit_first').disabled = false;
+            stopSpinner3();
             return false;
         }
     });
