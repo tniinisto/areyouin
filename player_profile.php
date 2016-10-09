405c2c3500e01d7005a40bb8961cb51c136ba72b
Tuomas <tniinisto@gmail.com>
2016-10-09 14:55:40 +0300

    back

--- player_profile.php
+++ player_profile.php
@@ -79,7 +79,7 @@
                         echo "<br />";
             mysql_close($con);
 ?>
-            <a href="#openModal" class="myButton" onclick="getPlayerProfile();">Edit information</a>
+            <a href="#openModal" class="myButton">Edit information</a>
             <!--<form action="#openModal">
                 <input type="submit" value="Edit information">
             </form>-->
