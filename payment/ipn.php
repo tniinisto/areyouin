9ef9d47d8d185636905929279f67fff895c7866e
Tuomas <tniinisto@gmail.com>
2016-10-12 00:15:44 +0300

    sandbox fsockopen adderess

--- payment/ipn.php
+++ payment/ipn.php
@@ -26,7 +26,8 @@
     $header .= "Content‐Type: application/x‐www‐form‐urlencoded\r\n";
     $header .= "Content‐Length: " . strlen($req) . "\r\n\r\n";
 
-    $fp = fsockopen ('ssl://www.paypal.com', 443, $errno, $errstr, 30);
+    //$fp = fsockopen ('ssl://www.paypal.com', 443, $errno, $errstr, 30);
+    $fp = fsockopen ('ssl://www.sandbox.paypal.com', 443, $errno, $errstr, 30);
 
     if (!$fp) {
 
