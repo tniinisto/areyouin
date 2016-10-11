cb4e923196d5ae33ce101a89bb265ed863ecf4be
Tuomas <tniinisto@gmail.com>
2016-10-11 22:45:02 +0300

    test

--- payment/ipnUsage.php
+++ payment/ipnUsage.php
@@ -1,7 +1,7 @@
 <?php 
 
-//require ("ipn.php");
-include ("ipn.php");
+require ("ipn.php");
+
 use PaypalIPN;
 
 $ipn = new PayPalIPN();
@@ -16,10 +16,9 @@ if ($verified) {
      */
      echo "verified test";
 }
+
 // Reply with an empty 200 response to indicate to paypal the IPN was received correctly.
-//echo "not verified test";
 
 header("HTTP/1.1 200 OK");
-
 
 ?>
