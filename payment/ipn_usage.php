d5a31887e4ff420df3bd0ee7ce9eb4cf81a434e8
Tuomas <tniinisto@gmail.com>
2016-10-11 22:04:22 +0300

    test ipn

--- payment/ipn_usage.php
+++ payment/ipn_usage.php
@@ -10,6 +10,7 @@ if ($verified) {
      * A list of variables is available here:
      * https://developer.paypal.com/webapps/developer/docs/classic/ipn/integration-guide/IPNandPDTVariables/
      */
+     echo "verified test";
 }
 // Reply with an empty 200 response to indicate to paypal the IPN was received correctly.
 header("HTTP/1.1 200 OK");
