66f59cb16ada18587438eb7d9b48ac563d654c49
Tuomas <tniinisto@gmail.com>
2016-10-11 22:18:59 +0300

    test

--- payment/ipn_usage.php
+++ payment/ipn_usage.php
@@ -13,6 +13,7 @@ if ($verified) {
      echo "verified test";
 }
 // Reply with an empty 200 response to indicate to paypal the IPN was received correctly.
+echo "not verified test";
 header("HTTP/1.1 200 OK");
 
 
