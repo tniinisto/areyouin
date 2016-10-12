bd93bb0444098b3e684690b75c6303533d652c71
Tuomas <tniinisto@gmail.com>
2016-10-12 18:07:22 +0300

    header

--- payment/ipn.php
+++ payment/ipn.php
@@ -28,6 +28,7 @@
     // $header .= "Content‐Length: " . strlen($req) . "\r\n\r\n";
     $header = "POST /cgi-bin/webscr HTTP/1.0\r\n";
     $header .= "Host: www.sandbox.paypal.com:443\r\n";
+    // $header .= "Host: www.paypal.com:443\r\n";
     $header .= "Content-Type: application/x-www-form-urlencoded\r\n";
     $header .= "Content-Length: " . strlen($req) . "\r\n\r\n";
 
