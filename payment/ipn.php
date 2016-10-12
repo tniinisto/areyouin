39a978013db79d2dc94ac8b68d01a4b717312819
Tuomas <tniinisto@gmail.com>
2016-10-12 14:57:45 +0300

    _notity-validate back to original

--- payment/ipn.php
+++ payment/ipn.php
@@ -16,13 +16,11 @@
     // $result = mysql_query($sql);
 
     // read the post from PayPal system and add 'cmd_notify‐validate' to the end
-    $validate = 'cmd=_notify‐validate';
-    $req = '';
+    $req = 'cmd=_notify‐validate';
     foreach ($_POST as $key => $value) {
         $value = urlencode(stripslashes($value));
         $req .= "&$key=$value";
     }
-    $req .= $validate;
 
     // post back to PayPal system to validate
     $header = "POST /cgi‐bin/webscr HTTP/1.0\r\n";
