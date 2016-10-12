1b8306dde210f9122bf25a1b5614814412ed72bc
Tuomas <tniinisto@gmail.com>
2016-10-12 21:50:38 +0300

    certs

--- payment/ipn.php
+++ payment/ipn.php
@@ -27,15 +27,15 @@
     // read the IPN message sent from PayPal and prepend 'cmd=_notify-validate'
     $req = 'cmd=_notify-validate';
     if (function_exists('get_magic_quotes_gpc')) {
-    $get_magic_quotes_exists = true;
+        $get_magic_quotes_exists = true;
     }
     foreach ($myPost as $key => $value) {
-    if ($get_magic_quotes_exists == true && get_magic_quotes_gpc() == 1) {
-        $value = urlencode(stripslashes($value));
-    } else {
-        $value = urlencode($value);
-    }
-    $req .= "&$key=$value";
+        if ($get_magic_quotes_exists == true && get_magic_quotes_gpc() == 1) {
+            $value = urlencode(stripslashes($value));
+        } else {
+            $value = urlencode($value);
+        }
+        $req .= "&$key=$value";
     }
 
     // Step 2: POST IPN data back to PayPal to validate
@@ -53,10 +53,12 @@
     // please download 'cacert.pem' from "http://curl.haxx.se/docs/caextract.html" and set
     // the directory path of the certificate as shown below:
     // curl_setopt($ch, CURLOPT_CAINFO, dirname(__FILE__) . '/cacert.pem');
+    curl_setopt($ch, CURLOPT_CAINFO, __DIR__ . "/cert/cacert.pem");
+    
     if ( !($res = curl_exec($ch)) ) {
-    // error_log("Got " . curl_error($ch) . " when processing IPN data");
-    curl_close($ch);
-    exit;
+        // error_log("Got " . curl_error($ch) . " when processing IPN data");
+        curl_close($ch);
+        exit;
     }
     curl_close($ch);
 
