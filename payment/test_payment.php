b6e1714260cae2f12f957dd16ae24a8d2379430c
Tuomas <tniinisto@gmail.com>
2016-10-14 19:34:49 +0300

    test

--- payment/test_payment.php
+++ payment/test_payment.php
@@ -137,5 +137,23 @@
 
 </script>
 
+
+<?php
+
+    $amount=$_GET["amount"];
+    $from=$_GET["from"];
+    $to=$_GET["to"];
+
+    function convertCurrency($amount, $from, $to){
+        $url  = "https://www.google.com/finance/converter?a=$amount&from=$from&to=$to";
+        $data = file_get_contents($url);
+        preg_match("/<span class=bld>(.*)<\/span>/",$data, $converted);
+        $converted = preg_replace("/[^0-9.]/", "", $converted[1]);
+        //return 'Euros set:' . $amount . ' and result in ' . $to . ': ' .  round($converted, 2);
+        return  round($converted, 2) . ',' .  $to;
+    }
+
+?>
+
 </body>
 
