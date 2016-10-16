edfac76941617352cef5461cc11274350199dbbd
Tuomas <tniinisto@gmail.com>
2016-10-16 18:55:21 +0300

    license to admin child navigation

--- players_insert.php
+++ players_insert.php
@@ -40,6 +40,7 @@
 				    echo "<li id='link_admingame' class='current2' onclick='showInsertPlayers();'><a href='#'>New event</a></li>";
                     echo "<li id='link_adminmembers'><a href='#'>Users</a></li>";
                     echo "<li id='link_adminsettings' onClick='setTimeout(function(){ initializeMap(); }, 100);'><a href='#'>Settings</a></li>";
+                    echo "<li id='link_license'><a href='#'>License</a></li>";
 			    echo "</ul>";
 		    echo "</nav>";
             //Navigation///////////////////////////////////////////////////////////////////////////
