4ef12479eb2678e1f23fae8c950ea7892d781076
Tuomas <tniinisto@gmail.com>
2016-10-16 14:03:02 +0300

    single payment button

--- payment/onlineCurrency.php
+++ payment/onlineCurrency.php
@@ -34,10 +34,11 @@ echo "<body>";
 
 //echo "<p>The team from session: " .  $_SESSION['myteamname'] . "</p>";
 
-echo "<div id='parent-container'>";
+//Subscription buttons////////////////////////////////////////////////////////////////////////////////////////////////////////
+echo "<div id='parent-container1' class='parent-container'>";
 
-    echo "<p>Make recurring monthly subscription.</p>";
-    
+    echo "<p>Make recurring monthly subscription</p>";
+
     echo "<div id='button1' class='child-container'>";
 
         echo"<form action='https://www.sandbox.paypal.com/cgi-bin/webscr' method='post' target='_top'>";
@@ -114,7 +115,7 @@ echo "<div id='parent-container'>";
     
      echo "</div>";    
     
-        echo "<div id='button2' class='child-container'>";
+        echo "<div id='button3' class='child-container'>";
 
         echo"<form action='https://www.sandbox.paypal.com/cgi-bin/webscr' method='post' target='_top'>";
         echo"<input type='hidden' name='cmd' value='_xclick-subscriptions'>";
@@ -151,60 +152,40 @@ echo "<div id='parent-container'>";
         echo"</form>";
     
      echo "</div>";  
- 
-    //     //Production
-    //     //echo "<form action='https://www.paypal.com/cgi-bin/webscr' method='post' target='_top'>";
-        
-    //     //Sandbox
-    //     echo "<form action='https://www.sandbox.paypal.com/cgi-bin/webscr' method='post' target='_top'>";
-    //     echo "<input type='hidden' name='cmd' value='_xclick-subscriptions'>";
-    //     echo "<input type='hidden' name='business' value='8RLTCNLYYQKLQ'>";
-    //     echo "<input type='hidden' name='lc' value='FI'>";
-    //     echo "<input type='hidden' name='item_name' value='RYouIN license'>";
-    //     echo "<input type='hidden' name='no_note' value='1'>";
-    //     echo "<input type='hidden' name='no_shipping' value='1'>";
-    //     echo "<input type='hidden' name='rm' value='1'>";
-    //     echo "<input type='hidden' name='return' value='https://dev-areyouin.azurewebsites.net/payment/success.html'>";
-    //     echo "<input type='hidden' name='cancel_return' value='https://dev-areyouin.azurewebsites.net/payment/cancel.html'>";
-    //     echo "<input type='hidden' name='src' value='1'>";
-    //     echo "<input type='hidden' name='currency_code' value='EUR'>";
-    //     echo "<input type='hidden' name='bn' value='PP-SubscriptionsBF:btn_subscribeCC_LG.gif:NonHosted'>";
-    //     echo "<table>";
-    //     echo "<tr><td id='currency_select'><input type='hidden' name='on0' value='Currency options'>Select currency</td></tr><tr><td><select name='os0'>";
-    //         echo "<option value='EUR' id='EUR'>EUR : €" . $price . " EUR - monthly</option>";
-    //         echo "<option value='USD' id='USD'>USD : $" . convertCurrency($price, 'EUR', 'USD') . " USD - monthly</option>";
-    //         echo "<option value='GBP' id='GBP'>GBP : £" . convertCurrency($price, 'EUR', 'GBP') . " GBP - monthly</option>";
-    //     echo "</select> </td></tr>";
-    //     echo "</table>";
-    //     echo "<br>";
-    //     echo "<input type='hidden' name='currency_code' value='EUR'>";
-    //     echo "<input type='hidden' name='option_select0' value='EUR'>";
-    //     echo "<input type='hidden' name='option_amount0' value='" . $price . "'>";
-    //     echo "<input type='hidden' name='option_period0' value='M'>";
-    //     echo "<input type='hidden' name='option_frequency0' value='1'>";
-    //     echo "<input type='hidden' name='option_select1' value='USD'>";
-    //     echo "<input type='hidden' name='option_amount1' value='" . convertCurrency(7.00, 'EUR', 'USD') . "'>";
-    //     echo "<input type='hidden' name='option_period1' value='M'>";
-    //     echo "<input type='hidden' name='option_frequency1' value='1'>";
-    //     echo "<input type='hidden' name='option_select2' value='GBP'>";
-    //     echo "<input type='hidden' name='option_amount2' value='" . convertCurrency(7.00, 'EUR', 'GBP') . "'>";
-    //     echo "<input type='hidden' name='option_period2' value='M'>";
-    //     echo "<input type='hidden' name='option_frequency2' value='1'>";
-    //     echo "<input type='hidden' name='option_index' value='0'>";
-    //     echo "<input type='image' src='https://www.sandbox.paypal.com/en_US/i/btn/btn_subscribeCC_LG.gif' border='0' name='submit' alt='PayPal - The safer, easier way to pay online!'>";
-    //     echo "<img alt='' border='0' src='https://www.sandbox.paypal.com/en_US/i/scr/pixel.gif' width='1' height='1'>"; 
-    //     echo "</form>";
-    // echo "</div>";
-
-    // echo "<div id='button1' class='child-container'>";
-
-    //     //Production
-    //     //echo "<form action='https://www.paypal.com/cgi-bin/webscr' method='post' target='_top'>";
     
 echo "</div>";
 
-echo "</body>";
+//Single pay buttons////////////////////////////////////////////////////////////////////////////////////////////////////////
+echo "<div id='parent-container1' class='parent-container'>";
+    
+    echo "<p>Make single month payment</p>";
 
+    echo "<div id='button4' class='child-container'>";
+
+         echo "<p>€ " . $price . " EUR</p>";
+
+         echo "<form action='https://www.sandbox.paypal.com/cgi-bin/webscr' method='post' target='_top'>                                                                                  ";
+         echo "    <input type='hidden' name='cmd' value='_xclick'>                                                                                                                       ";
+         echo "    <input type='hidden' name='business' value='8RLTCNLYYQKLQ'>                                                                                                            ";
+         echo "    <input type='hidden' name='lc' value='FI'>                                                                                                                             ";
+         echo "    <input type='hidden' name='item_name' value='R'YouIN Test'>                                                                                                            ";
+         echo "    <input type='hidden' name='amount' value=' € " . $price . " EUR'>                                                                                                                       ";
+         echo "    <input type='hidden' name='currency_code' value='EUR'>                                                                                                                 ";
+         echo "    <input type='hidden' name='button_subtype' value='services'>                                                                                                           ";
+         echo "    <input type='hidden' name='no_note' value='0'>                                                                                                                         ";
+         echo "    <input type='hidden' name='cn' value='Add special instructions to the seller:'>                                                                                        ";
+         echo "    <input type='hidden' name='no_shipping' value='2'>                                                                                                                     ";
+         echo "    <input type='hidden' name='bn' value='PP-BuyNowBF:btn_buynowCC_LG.gif:NonHosted'>                                                                                      ";
+         echo "    <input type='image' src='https://www.sandbox.paypal.com/en_US/i/btn/btn_buynowCC_LG.gif' border='0' name='submit' alt='PayPal - The safer, easier way to pay online!'> ";
+         echo "    <img alt='' border='0' src='https://www.sandbox.paypal.com/en_US/i/scr/pixel.gif' width='1' height='1'>                                                                ";
+         echo "</form>                                                                                                                                                                    ";
+
+    echo "</div>";
+
+echo "</div>";
+
+
+echo "</body>";
 echo "</html>";
 
 ?>
