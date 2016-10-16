3d4ce42701d2dabb1ece007131af8a7321858cb9
Tuomas <tniinisto@gmail.com>
2016-10-16 20:42:28 +0300

    test

--- players_insert.php
+++ players_insert.php
@@ -725,13 +725,13 @@
                 /////////////////////////////////////////////////////////////////////////////////////////////
                 echo "<div id='license_content_id' class='noshow'>";
                     
-                    echo "<h2>License coming here</h2>";
+                    echo "<h2>License</h2>";
 
                     ob_start(); // begin collecting output
                     include 'payment/onlineCurrency.php';
                     $result = ob_get_clean(); // retrieve output from myfile.php, stop buffering
                     echo $result;
-                    
+
                 echo "</div>";
 
             echo "</article>";
