6ec9a876719fa5b2c950ad59af8f500a16bf32d2
Tuomas <tniinisto@gmail.com>
2016-10-18 19:41:21 +0300

    team creation test

--- register/js/newTeam.php
+++ register/js/newTeam.php
@@ -61,23 +61,23 @@
         
         //Create team/////////////////////////////////////////////////////////////////////
         
-            // //Calculate offset to UTC//////////////////////
-            // date_default_timezone_set( "UTC" );    
-            // $daylight_savings_offset_in_seconds = timezone_offset_get( timezone_open($_GET['timezone']), new DateTime() ); 
-            // $offset = round($daylight_savings_offset_in_seconds/3600); //Hours
+            //Calculate offset to UTC//////////////////////
+            date_default_timezone_set( "UTC" );    
+            $daylight_savings_offset_in_seconds = timezone_offset_get( timezone_open($_GET['timezone']), new DateTime() ); 
+            $offset = round($daylight_savings_offset_in_seconds/3600); //Hours
             
-            // $sql3 = "INSERT INTO team (teamname, timezone, utcoffset, maxplayers, inuse) VALUES (:teamname, :timezone, :utcoffset, :maxplayers, :inuse)";
+            $sql3 = "INSERT INTO team (teamname, timezone, utcoffset, maxplayers, inuse) VALUES (:teamname, :timezone, :utcoffset, :maxplayers, :inuse)";
 
-            // if($_SESSION['ChromeLog']) { ChromePhp::log('Create new team: ' . $sql3); }
+            if($_SESSION['ChromeLog']) { ChromePhp::log('Create new team: ' . $sql3); }
             
-            // $stmt3 = $dbh->prepare($sql3);
-            // $stmt3->bindParam(':teamname', $_GET['teamname'], PDO::PARAM_STR);
-            // $stmt3->bindParam(':timezone', $_GET['timezone'], PDO::PARAM_STR);
-            // $stmt3->bindParam(':utcoffset', $offset, PDO::PARAM_INT);
-            // $stmt3->bindParam(':maxplayers', '20', PDO::PARAM_INT);
-            // $stmt3->bindParam(':inuse', '1', PDO::PARAM_INT);        
+            $stmt3 = $dbh->prepare($sql3);
+            $stmt3->bindParam(':teamname', $_GET['teamname'], PDO::PARAM_STR);
+            $stmt3->bindParam(':timezone', $_GET['timezone'], PDO::PARAM_STR);
+            $stmt3->bindParam(':utcoffset', $offset, PDO::PARAM_INT);
+            $stmt3->bindParam(':maxplayers', 20, PDO::PARAM_INT);
+            $stmt3->bindParam(':inuse', 1, PDO::PARAM_INT);        
             
-            // $result3 = $stmt3->execute();
+            $result3 = $stmt3->execute();
 
 
             // //Get the team////////////////////////////////////////////////
