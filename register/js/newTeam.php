6f0d5e4b322d2d648225ca2e49f1a80184815908
Tuomas <tniinisto@gmail.com>
2016-10-19 16:57:44 +0300

    comment playerid to 0

--- register/js/newTeam.php
+++ register/js/newTeam.php
@@ -131,8 +131,8 @@
 
         $comment = "Welcome to your chat!";
         $stmt7->bindParam(':comment', $comment, PDO::PARAM_STR);
+        $stmt7->bindParam(':playerid', 0, PDO::PARAM_INT);
         $stmt7->bindParam(':teamid', $teamid_max, PDO::PARAM_INT);
-        $stmt7->bindParam(':playerid', $registrar, PDO::PARAM_INT);
         $stmt7->bindParam(':insertdate', $insertDate, PDO::PARAM_STR);
 
         $result7 = $stmt7->execute();   
