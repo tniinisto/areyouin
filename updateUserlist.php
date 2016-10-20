d9a60fe3feb4736ae7c8203779cd1c80025f6e87
Tuomas <tniinisto@gmail.com>
2016-10-20 13:29:52 +0300

    buttons, registrar and normal user

--- updateUserlist.php
+++ updateUserlist.php
@@ -263,14 +263,15 @@
                                                 
                                                 echo "<div class='buttonHolder' style='margin-top:15px;'>";
 
-                                                    echo "<input type='button' class='myButton' style='float: left; margin-left: 30px;' value='Save'
+                                                    if($player->registrar == 0) { //Dont let delete Registrar
+                                                        echo "<input type='button' class='myButton' style='float: left; margin-left: 30px;' value='Save'
                                                         onclick='updateAdminStatus(" . $player->playerID . ", \"dialog_admin_switch". $index . "\");'/>";
 
-                                                    if($player->registrar == 0) { //Dont let delete Registrar
                                                         echo "<input type='button' class='myButton' style='color: red; float: rigth;' value='Delete'
                                                         onclick='confirmDelete(" . $player->playerID . ");'/>";
                                                     } else {
-                                                        echo "<input type='button' class='myButton' style='visibility: hidden; color: red; float: rigth;' value='Delete'/>";
+                                                        echo "<input type='button' class='myButton' style='float: left; margin-left: 30px;' value='Save'
+                                                        onclick='updateAdminStatus(" . $player->playerID . ", \"dialog_admin_switch". $index . "\");'/>";
                                                     }
 
                                                 echo "</div>";
