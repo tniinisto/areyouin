3eca914276fc111bb1f22434b756bb490dd1bf95
Tuomas <tniinisto@gmail.com>
2016-10-22 11:31:57 +0300

    RYouIN player for the first chat

--- register/js/newTeam.php
+++ register/js/newTeam.php
@@ -129,9 +129,10 @@
         
         $stmt7 = $dbh->prepare($sql7);
 
+        $ryouin_player = 0;
         $comment = "Welcome to R'YouIN chat! This is an automatic message from the team's registration.";
         $stmt7->bindParam(':comment', $comment, PDO::PARAM_STR);
-        $stmt7->bindParam(':playerid', $playerid, PDO::PARAM_INT);
+        $stmt7->bindParam(':playerid', $ryouin_player, PDO::PARAM_INT);
         $stmt7->bindParam(':teamid', $teamid_max, PDO::PARAM_INT);
         $stmt7->bindParam(':insertdate', $insertDate, PDO::PARAM_STR);
 
