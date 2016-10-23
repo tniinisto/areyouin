1307adc81acb6595fb8c12673e9d396cbedc17ef
tniinisto <tniinisto@gmail.com>
2016-10-23 10:01:19 +0300

    one sql for team info

--- setTeam.php
+++ setTeam.php
@@ -25,34 +25,35 @@
     try {
 
         //Check & set team admin status for user////////////////////////////////////////////////////////////////
-        $sql1 = "SELECT teamAdmin FROM playerteam, team where Team_teamID = :teamid AND Players_playerID = :player";
+        // $sql1 = "SELECT teamAdmin FROM playerteam, team where Team_teamID = :teamid AND Players_playerID = :player";
 
-        if($_SESSION['ChromeLog']) { ChromePhp::log('set admin status: ' . $sql1); }
+        // if($_SESSION['ChromeLog']) { ChromePhp::log('set admin status: ' . $sql1); }
         
-        $stmt1 = $dbh->prepare($sql1);
-        $stmt1->bindParam(':player', $_SESSION['myplayerid'], PDO::PARAM_INT);
-        $stmt1->bindParam(':teamid', $_SESSION['myteamid'], PDO::PARAM_INT);
-        $stmt1->execute();
-        $row1 = $stmt1->fetch();
+        // $stmt1 = $dbh->prepare($sql1);
+        // $stmt1->bindParam(':player', $_SESSION['myplayerid'], PDO::PARAM_INT);
+        // $stmt1->bindParam(':teamid', $_SESSION['myteamid'], PDO::PARAM_INT);
+        // $stmt1->execute();
+        // $row1 = $stmt1->fetch();
 
-        //Set chosen teams admin status  to session
-        $_SESSION['myAdmin'] = $row1['teamAdmin'];
+        // //Set chosen teams admin status  to session
+        // $_SESSION['myAdmin'] = $row1['teamAdmin'];
 
 
-
-        //Set rest of team specific session variables/////////////////////////////////////////////////////////////
-        $sql2 = "SELECT teamID, teamName, timezone, utcOffset, registrar, lastMsg
+        //Set player & team specific session variables/////////////////////////////////////////////////////////////
+        $sql2 = "SELECT teamAdmin, teamID, teamName, timezone, utcOffset, registrar, lastMsg
                 FROM playerteam m, team t
-                WHERE m.Team_teamID = t.teamid AND teamID = :teamid;";
+                WHERE m.Team_teamID = t.teamid AND teamID = :teamid AND Players_playerID = :player;";
 
 
         if($_SESSION['ChromeLog']) { ChromePhp::log('set admin status: ' . $sql2); }
         
         $stmt2 = $dbh->prepare($sql2);
         $stmt2->bindParam(':teamid', $_SESSION['myteamid'], PDO::PARAM_INT);
+        $stmt1->bindParam(':player', $_SESSION['myplayerid'], PDO::PARAM_INT);
         $stmt2->execute();
         $row2 = $stmt2->fetch();        
 
+        $_SESSION['myAdmin'] = $row2['teamAdmin'];
         $_SESSION['myteamid'] = $row2['teamID'];
         $_SESSION['myteamname'] = $row2['teamName'];
         $_SESSION['myRegistrar'] = $row2['registrar'];
