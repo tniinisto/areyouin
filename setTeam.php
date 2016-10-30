0bf24eb6e9e30fba43a96cdb49f3a386e991866d
Tuomas <tniinisto@gmail.com>
2016-10-30 14:01:02 +0200

    no date format

--- setTeam.php
+++ setTeam.php
@@ -49,11 +49,11 @@
         
         //UTC//
         date_default_timezone_set("UTC");
-        
+
         $licenseValid = new DateTime($row2['licensevalid']);
-        $licenseValid = $licenseValid->format('Y-n-j');
+        //$licenseValid = $licenseValid->format('Y-n-j');
         $currentDate = new DateTime('now');
-        $currentDate = $currentDate->format('Y-n-j');
+        //$currentDate = $currentDate->format('Y-n-j');
 
         if($currentDate > $licenseValid)
             header('Location:licenseExpired.php');    
