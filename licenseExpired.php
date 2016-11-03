86779c949bd6ffecd8340af2359c3e2039d604bf
Tuomas <tniinisto@gmail.com>
2016-11-03 13:28:37 +0200

    layouting

--- licenseExpired.php
+++ licenseExpired.php
@@ -28,7 +28,9 @@ echo "<article id='license_expried_article' class='clearfix license_exp'>";
 
         echo "<p style='text-align: center; font-size: initial; font-weight: bold; margin-bottom: 40px;'>Your R'YouIN usage license has expired. You can purhcase a new license and continue usage immediately after the payment.</p>";
         
-        echo "<a href='default.html'>Back to login</a>";
+        echo "<div style='display: inline-block; text-align: center;'>";
+            echo "<a style='color: #474747; font-size: large; font-weight: bold;' href='default.html'>Back to login</a>";
+        echo "</div>";
 
         ob_start(); // begin collecting output
         include 'payment/onlineCurrency.php';
