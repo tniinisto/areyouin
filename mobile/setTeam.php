099e6c12b8f6cc477b986f944dfdae662043e1fe
Tuomas <tniinisto@gmail.com>
2016-11-03 13:11:22 +0200

    mobile login check with license support

--- mobile/setTeam.php
+++ mobile/setTeam.php
@@ -1,9 +1,70 @@
 <?php
-        session_start();
+    include( $_SERVER['DOCUMENT_ROOT'] . '/config/config.php' );
 
-        $teamid=$_POST['teamselect'];
-        $_SESSION['myteamid'] = $teamid;
+    session_start();
 
-        header("location:login_success.php");
+    if($_SESSION['ChromeLog']) {
+        require_once 'ChromePhp.php';
+        ChromePhp::log('setTeam.php start');
+    }
+
+    // No cache
+    header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+    header("Cache-Control: no-cache");
+    header("Pragma: no-cache");
+
+    //Set team info to session from select POST, get teamid and teamname
+    $teaminfo = explode("|", $_POST['teamselect']);
+    $_SESSION['myteamid'] = $teaminfo[0];
+    $_SESSION['myteamname'] = $teaminfo[1];
+
+
+  	$dbh = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);	
+	$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    try {
+
+        //Set player & team specific session variables/////////////////////////////////////////////////////////////
+        $sql2 = "SELECT teamAdmin, teamID, teamName, timezone, utcOffset, registrar, lastMsg, licenseValid
+                FROM playerteam m, team t, registration r
+                WHERE m.Team_teamID = t.teamid AND teamID = :teamid AND m.Players_playerID = :player AND r.team_teamid = t.teamid;";
+
+        if($_SESSION['ChromeLog']) { ChromePhp::log('set admin status: ' . $sql2); }
+        
+        $stmt2 = $dbh->prepare($sql2);
+        $stmt2->bindParam(':teamid', $_SESSION['myteamid'], PDO::PARAM_INT);
+        $stmt2->bindParam(':player', $_SESSION['myplayerid'], PDO::PARAM_INT);
+        $stmt2->execute();
+        $row2 = $stmt2->fetch();        
+
+        $_SESSION['myAdmin'] = $row2['teamAdmin'];
+        $_SESSION['myteamid'] = $row2['teamID'];
+        $_SESSION['myteamname'] = $row2['teamName'];
+        $_SESSION['myRegistrar'] = $row2['registrar'];
+        $_SESSION['mytimezone'] = $row2['timezone'];
+        $_SESSION['myoffset'] = $row2['utcOffset'];
+        $_SESSION['mylicense'] = $row2['licenseValid'];
+
+        //Check license status/////////////////////////////////
+        
+        //UTC//
+        date_default_timezone_set("UTC");
+
+        $licenseValid = new DateTime($row2['licenseValid']);
+        $licenseValid = $licenseValid->format('Y-n-j');
+        $currentDate = new DateTime('now');
+        $currentDate = $currentDate->format('Y-n-j');
+
+        if($currentDate > $licenseValid)
+            header('Location:licenseExpired.php');    
+        else
+            header('Location:login_success.php');        
+
+    }
+    catch(PDOException $e) {
+	    echo '{"error":{"text":'. $e->getMessage() .'}}'; 
+    }
   
+    $dbh = null;
+    
 ?>
