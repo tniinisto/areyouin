86cfe3ad251a6ad0703e8dfc024cd1fff459d0b5
Tuomas <tniinisto@gmail.com>
2016-11-08 12:47:57 +0200

    h3 texts back

--- players_insert.php
+++ players_insert.php
@@ -191,21 +191,21 @@
                 //Team users
                 echo "<div id='member_content_id' class='noshow'>";
                     echo "<div class='signal'>";
-                        //echo "<h3 style='text-align: center;'>Team members</h3>";
+                        echo "<h3 style='text-align: center;'>Team members</h3>";
                     echo "</div>";        
                 echo "</div>";
 
                 //Team settings
                 echo "<div id='team_content_id' class='noshow'>";
                     echo "<div class='signal'>";
-                        //echo "<h3 style='text-align: center;'>Team settings</h3>";
+                        echo "<h3 style='text-align: center;'>Team settings</h3>";
                     echo "</div>";
                 echo "</div>";
 
                 //Team license
                 echo "<div id='license_content_id' class='noshow'>";
                     echo "<div class='signal'>";
-                        //echo "<h3 style='text-align: center;'>Team license</h3>";
+                        echo "<h3 style='text-align: center;'>Team license</h3>";
                     echo "</div>";
                 echo "</div>";
                 
