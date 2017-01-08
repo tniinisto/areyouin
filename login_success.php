4a73110dde1a4b105459c95c52c43d8bbe4516d4
tniinisto <tniinisto@gmail.com>
2017-01-08 14:11:15 +0200

    cache  lines  off

--- login_success.php
+++ login_success.php
@@ -1,9 +1,9 @@
 <?php
     
     // No cache
-    header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
-    header("Cache-Control: no-cache");
-    header("Pragma: no-cache");
+    // header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+    // header("Cache-Control: no-cache");
+    // header("Pragma: no-cache");
 
     session_start();
 
