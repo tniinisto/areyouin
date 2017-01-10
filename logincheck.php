1eb1a32ba43b395d82b4e0c62e07e4da805bef7d
Tuomas <tniinisto@gmail.com>
2017-01-10 16:04:04 +0200

    sql fix

--- logincheck.php
+++ logincheck.php
@@ -69,7 +69,7 @@
 
     //The count
     $sql = "SELECT count(*) as count
-    FROM FROM players p, playerteam m, team t, registration r
+    FROM players p, playerteam m, team t, registration r
     WHERE (name = :name OR mail = :mail ) and password = :passmd5 and p.playerID = m.Players_playerID and m.Team_teamID = t.teamid and t.teamid <> 0 and r.team_teamid = t.teamid";
 
     $stmt = $dbh->prepare($sql);
