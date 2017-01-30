0a2bdad4df49d1ece4edbc5a2956fdb71f7b7d85
Tuomas <tniinisto@gmail.com>
2017-01-30 18:58:58 +0200

    css spinner

--- logincheck.php
+++ logincheck.php
@@ -92,7 +92,7 @@
 
             echo "<script type=\"text/javascript\" src=\"main.js\"> </script>";
             echo "<script src=\"js/jquery-2.0.0.min.js\"></script>";
-            echo "<script type='text/javascript' src='js/spin.min.js'></script>";            
+            //echo "<script type='text/javascript' src='js/spin.min.js'></script>";            
 
             echo "</head>";
 
@@ -103,7 +103,11 @@
                             echo "<h1 id='loginsite-logo' style='margin-top: 10px;'>R'YouIN</h1>";
                         echo "</div>";
                         //echo "<br />";
-                        echo "<div id='spinnerteamlogin_id' class='spin'></div>";
+                        //echo "<div id='spinnerteamlogin_id' class='spin'></div>";
+
+                        echo "<div class='signal'>";
+                        echo "</div>"; 
+
                         echo "<br />";
                         //echo "<br />";
                         echo "<fieldset id=\"loginfailfs\">";
@@ -126,39 +130,39 @@
                     echo "</div>";
                 echo "</div>";
 
-                echo "<script  type='text/javascript'>";
-                    echo "var spinnerTeamlogin;";
+                // echo "<script  type='text/javascript'>";
+                //     echo "var spinnerTeamlogin;";
 
-                    echo "var opts = {
-                       lines: 15 // The number of lines to draw
-                        , length: 2 // The length of each line
-                        , width: 4 // The line thickness
-                        , radius: 10 // The radius of the inner circle
-                        , scale: 1 // Scales overall size of the spinner
-                        , corners: 1 // Corner roundness (0..1)
-                        , color: '#fff' // #rgb or #rrggbb or array of colors
-                        , opacity: 0.25 // Opacity of the lines
-                        , rotate: 0 // The rotation offset
-                        , direction: 1 // 1: clockwise, -1: counterclockwise
-                        , speed: 1 // Rounds per second
-                        , trail: 60 // Afterglow percentage
-                        , fps: 20 // Frames per second when using setTimeout() as a fallback for CSS
-                        , zIndex: 2e9 // The z-index (defaults to 2000000000)
-                        , className: 'spinner' // The CSS class to assign to the spinner
-                        , top: '100px;' // Top position relative to parent
-                        , left: '50%' // Left position relative to parent
-                        , shadow: false // Whether to render a shadow
-                        , hwaccel: false // Whether to use hardware acceleration
-                        , position: 'fixed' // Element positioning
-                    };";
+                //     echo "var opts = {
+                //        lines: 15 // The number of lines to draw
+                //         , length: 2 // The length of each line
+                //         , width: 4 // The line thickness
+                //         , radius: 10 // The radius of the inner circle
+                //         , scale: 1 // Scales overall size of the spinner
+                //         , corners: 1 // Corner roundness (0..1)
+                //         , color: '#fff' // #rgb or #rrggbb or array of colors
+                //         , opacity: 0.25 // Opacity of the lines
+                //         , rotate: 0 // The rotation offset
+                //         , direction: 1 // 1: clockwise, -1: counterclockwise
+                //         , speed: 1 // Rounds per second
+                //         , trail: 60 // Afterglow percentage
+                //         , fps: 20 // Frames per second when using setTimeout() as a fallback for CSS
+                //         , zIndex: 2e9 // The z-index (defaults to 2000000000)
+                //         , className: 'spinner' // The CSS class to assign to the spinner
+                //         , top: '100px;' // Top position relative to parent
+                //         , left: '50%' // Left position relative to parent
+                //         , shadow: false // Whether to render a shadow
+                //         , hwaccel: false // Whether to use hardware acceleration
+                //         , position: 'fixed' // Element positioning
+                //     };";
 
-                    echo "spinnerTeamlogin = new Spinner(opts);";
+                //     echo "spinnerTeamlogin = new Spinner(opts);";
 
-                    echo "function startLoginSpinner() {";
-                        echo "var target = document.getElementById('spinnerteamlogin_id');";
-                        echo "spinnerTeamlogin.spin(target);";
-                    echo "}";
-                echo "</script>";
+                //     echo "function startLoginSpinner() {";
+                //         echo "var target = document.getElementById('spinnerteamlogin_id');";
+                //         echo "spinnerTeamlogin.spin(target);";
+                //     echo "}";
+                // echo "</script>";
 
             echo "</body>";
         echo "</html>";
