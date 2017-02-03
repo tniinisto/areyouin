8b3047c84b49cf37e2c7f1687372370fa482f4fb
Tuomas <tniinisto@gmail.com>
2017-02-03 14:55:53 +0200

    text tune

--- admin_team_settings.php
+++ admin_team_settings.php
@@ -52,7 +52,7 @@
                 echo "<fieldset id='timezone_set'>";
                     echo "<legend style='text-align: left;'><h2>Timezone</h2></legend>";
                     
-                    echo "<p style='text-align: center;'>Setting the correct timezone ensures that events are shown correctly for you.</p>";
+                    echo "<p style='text-align: center;'>By setting the correct timezone ensures that events and chat messages are shown correctly for your team.</p>";
 
                     echo "<div style='background-color: #b9b9b9; margin: 5px;'>";
                         echo "<h3 id='team_timezone' style='text-align: center;'>Current timezone:</h3>";
