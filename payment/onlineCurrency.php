d85d87479afb4ec5d3d273ead0bcdb19e95384b1
tniinisto <tniinisto@gmail.com>
2017-03-01 16:22:37 +0200

    VAT removed!!!

--- payment/onlineCurrency.php
+++ payment/onlineCurrency.php
@@ -134,7 +134,7 @@ echo "<div id='parent-container1' class='parent-container'>";
             echo "<input type='hidden' name='no_shipping' value='1'>"; //Value set to 1 in production
             echo "<input type='hidden' name='rm' value='1'>";           
             echo "<input type='hidden' name='bn' value='PP-BuyNowBF:btn_buynowCC_LG.gif:NonHosted'>";
-            echo "<input type='hidden' name='tax_rate' value='24.000'>";
+            //echo "<input type='hidden' name='tax_rate' value='24.000'>";
             echo "<input type='hidden' name='return' value='https://r-youin.com/payment/success.html'>";
             echo "<input type='hidden' name='cancel_return' value='https://r-youin.com/payment/cancel.html'>";
         
@@ -169,7 +169,7 @@ echo "<div id='parent-container1' class='parent-container'>";
             echo "<input type='hidden' name='no_shipping' value='1'>"; 
             echo "<input type='hidden' name='rm' value='1'>";           
             echo "<input type='hidden' name='bn' value='PP-BuyNowBF:btn_buynowCC_LG.gif:NonHosted'>";
-            echo "<input type='hidden' name='tax_rate' value='24.000'>";
+            //echo "<input type='hidden' name='tax_rate' value='24.000'>";
             echo "<input type='hidden' name='return' value='https://r-youin.com/payment/success.html'>";
             echo "<input type='hidden' name='cancel_return' value='https://r-youin.com/payment/cancel.html'>";            
         
@@ -203,7 +203,7 @@ echo "<div id='parent-container1' class='parent-container'>";
             echo "<input type='hidden' name='no_shipping' value='1'>";
             echo "<input type='hidden' name='rm' value='1'>";                
             echo "<input type='hidden' name='bn' value='PP-BuyNowBF:btn_buynowCC_LG.gif:NonHosted'>";
-            echo "<input type='hidden' name='tax_rate' value='24.000'>";
+            //echo "<input type='hidden' name='tax_rate' value='24.000'>";
             echo "<input type='hidden' name='return' value='https://r-youin.com/payment/success.html'>";
             echo "<input type='hidden' name='cancel_return' value='https://r-youin.com/payment/cancel.html'>";    
                     
@@ -238,7 +238,7 @@ echo "<div id='parent-container1' class='parent-container'>";
             echo "<input type='hidden' name='no_shipping' value='1'>";   
             echo "<input type='hidden' name='rm' value='1'>";         
             echo "<input type='hidden' name='bn' value='PP-BuyNowBF:btn_buynowCC_LG.gif:NonHosted'>";
-            echo "<input type='hidden' name='tax_rate' value='24.000'>";
+            //echo "<input type='hidden' name='tax_rate' value='24.000'>";
             echo "<input type='hidden' name='return' value='https://r-youin.com/payment/success.html'>";
             echo "<input type='hidden' name='cancel_return' value='https://r-youin.com/payment/cancel.html'>"; 
         
@@ -271,7 +271,7 @@ echo "<div id='parent-container1' class='parent-container'>";
             echo "<input type='hidden' name='no_shipping' value='1'>";
             echo "<input type='hidden' name='rm' value='1'>";              
             echo "<input type='hidden' name='bn' value='PP-BuyNowBF:btn_buynowCC_LG.gif:NonHosted'>";
-            echo "<input type='hidden' name='tax_rate' value='24.000'>";
+            //echo "<input type='hidden' name='tax_rate' value='24.000'>";
             echo "<input type='hidden' name='return' value='https://r-youin.com/payment/success.html'>";
             echo "<input type='hidden' name='cancel_return' value='https://r-youin.com/payment/cancel.html'>";             
         
@@ -303,7 +303,7 @@ echo "<div id='parent-container1' class='parent-container'>";
             //echo "<input type='hidden' name='cn' value='Add special instructions to the seller:'>";
             echo "<input type='hidden' name='no_shipping' value='1'>";            
             echo "<input type='hidden' name='bn' value='PP-BuyNowBF:btn_buynowCC_LG.gif:NonHosted'>";
-            echo "<input type='hidden' name='tax_rate' value='24.000'>";
+            //echo "<input type='hidden' name='tax_rate' value='24.000'>";
             echo "<input type='hidden' name='return' value='https://r-youin.com/payment/success.html'>";
             echo "<input type='hidden' name='cancel_return' value='https://r-youin.com/payment/cancel.html'>";            
                     
@@ -340,7 +340,7 @@ echo "<div id='parent-container1' class='parent-container'>";
 
 echo "</div>";
 
-echo "<div style='text-align: center'><label>24% VAT is added to prices on payment. Tax is separated on the receipt.</label> </div>";
+//echo "<div style='text-align: center'><label>24% VAT is added to prices on payment. Tax is separated on the receipt.</label> </div>";
 
 $dbh = null;
 
