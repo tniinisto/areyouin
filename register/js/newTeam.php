7ee333b9675d5f788f184f44da7523d6ef735fe3
tniinisto <tniinisto@gmail.com>
2017-03-09 18:22:38 +0200

    three month free period

--- register/js/newTeam.php
+++ register/js/newTeam.php
@@ -144,7 +144,8 @@
         $registrationDate = date("Y-n-j H:i:s");
 
         $start = new DateTimeImmutable($registrationDate);
-        $valid = $start->modify('+14 day');
+        //$valid = $start->modify('+14 day');
+        $valid = $start->modify('+90 day');
         $valid = $valid->format('Y-m-d H:i:s');
 
         $sql8 = "INSERT INTO registration (registered, players_playerid, team_teamid, licensevalid) VALUES (:registered, :playerid, :teamid, :licensevalid)";
