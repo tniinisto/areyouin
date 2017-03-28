b99f8d1add15625c3d09ed5594f205fc964d94a9
tniinisto <tniinisto@gmail.com>
2017-03-28 16:40:59 +0300

    another way

--- PGmobile/login_success.php
+++ PGmobile/login_success.php
@@ -1,32 +1,37 @@
 <?php     
     session_start();
 
-    if($_SESSION['ChromeLog']) {
-        require_once 'ChromePhp.php';
-        ChromePhp::log('login_success.php, start...');
-    }
-
     // Check if session is not registered, redirect back to main page. 
     // Put this code in first line of web page. 
 
-    if(!$_SESSION['myusername']){
+    //if(!$_SESSION['myusername']){
 
-        if($_SESSION['ChromeLog']) { ChromePhp::log('login_success.php, not session_is_registered'); }
+        //if($_SESSION['ChromeLog']) { ChromePhp::log('login_success.php, not session_is_registered'); }
 
         //header("location:default.php");
-    }
-    else {
-        if($_SESSION['ChromeLog']) { ChromePhp::log('login_success.php, session_is_registered'); }
+    //}
+    //else {
+        //if($_SESSION['ChromeLog']) { ChromePhp::log('login_success.php, session_is_registered'); }
 
         //header("location:index.html");
 
-        $index = file_get_contents('index.html');
-        echo $index;
+        //$index = file_get_contents('index.html');
+        //echo $index;
 
         // ob_start(); // begin collecting output
         // include 'index.html';
         // $result = ob_get_clean(); // retrieve output from myfile.php, stop buffering
-        // echo $result;       
+        // echo $result;       <html>
+
+        echo "<head>";
+        echo "    <script type='text/javascript'>";
+        echo "        window.location.href = 'index.html';";
+        echo "    </script>";
+        echo "</head>";
+        echo "<body>";
+        echo "</body>";
+        echo "</html>";
+
     }
 
 ?>
