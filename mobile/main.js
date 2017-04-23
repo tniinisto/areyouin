1085a24973097d65cace9e07d43ebace7edc22c0
tniinisto <tniinisto@gmail.com>
2017-04-23 18:15:31 +0300

    toLoginPage() updated to main.js

--- mobile/main.js
+++ mobile/main.js
@@ -704,14 +704,20 @@ function refreshScroll() {
     });    
 }
 
-// function toLoginPage() {
-//     var loginURL = window.location.href;
-//     loginURL = loginURL.substring(0, loginURL.lastIndexOf('/') + 1);
-//     loginURL = loginURL + "default.php";
-//     //alert(loginURL);
+function toLoginPage() {
+    if (navigator.app && navigator.app.exitApp) {
+        navigator.app.exitApp();
+    } else if (navigator.device && navigator.device.exitApp) {
+        navigator.device.exitApp();
+    } else {
 
-//     window.location.assign(loginURL);
-// }
+        var loginURL = window.location.href;
+        loginURL = loginURL.substring(0, loginURL.lastIndexOf('/') + 1);
+        loginURL = loginURL + "default.php";
+    
+        window.location.assign(loginURL);      
+    }  
+}
 
 //function sendMail() {
 //    alert("sendMail()");
