a7677bc4d24846f93d95592669738e16661356a8
tniinisto <tniinisto@gmail.com>
2017-04-29 16:20:22 +0300

    dev config

---
+++ PGmobile/services/config.php
@@ -0,0 +1,22 @@
+<?php
+	//Production Database credentials 
+     	$dbhost = 'eu-cdbr-azure-north-a.cloudapp.net';
+	$dbuser = 'bd3d44ed2e1c4a';
+	$dbpass = '8ffac735';
+	$dbname = 'areyouin';
+	
+	//Dev Database credentials
+	//$dbhost = 'eu-cdbr-azure-north-e.cloudapp.net';
+	//$dbuser = 'b4d91392f2a7cb';
+	//$dbpass = '7ff800ca';
+	//$dbname = 'dev_areyouin';
+
+    //SITES
+    $home_master = 'http://areyouin.azurewebsites.net/';
+    $home_dev = 'http://dev-areyouin.azurewebsites.net/';
+    $home_mobile = 'http://m-areyouin.azurewebsites.net/';
+
+    //Sendgrid mail credentials
+    $mail_user = 'azure_0d3507e7b5b2d3634624f1bcaba9b3fe@azure.com';
+    $mail_key = 'WjL725aJL225Kk';
+?>
