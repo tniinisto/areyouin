ec705af7418b92cfea7776ef2aa4a9febd925b6f
tniinisto <tniinisto@gmail.com>
2017-04-29 16:20:56 +0300

    tune

--- PGmobile/services/config.php
+++ PGmobile/services/config.php
@@ -1,6 +1,6 @@
 <?php
 	//Production Database credentials 
-     	$dbhost = 'eu-cdbr-azure-north-a.cloudapp.net';
+    $dbhost = 'eu-cdbr-azure-north-a.cloudapp.net';
 	$dbuser = 'bd3d44ed2e1c4a';
 	$dbpass = '8ffac735';
 	$dbname = 'areyouin';
