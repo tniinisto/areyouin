99b719dd800d64089a33be783ffa74c7e391febf
tniinisto@gmail.com <tniinsto@gmail.com>
2017-06-07 19:10:53 +0300

    mail address to login checking

--- PGmobile/services/login.php
+++ PGmobile/services/login.php
@@ -27,8 +27,8 @@ $sql = "SELECT x.count, p.playerID, p.name, t.teamID, t.teamName, m.teamAdmin, m
     FROM players p, playerteam m, team t, registration r,         
         (SELECT count(*) as count
         FROM players p, playerteam m, team t, registration r
-        WHERE name = '$name' and password = '$mymd5' and p.playerID = m.Players_playerID and m.Team_teamID = t.teamid and t.teamid <> 0 and r.team_teamid = t.teamid) as x
-    WHERE name = '$name' and password = '$mymd5' and p.playerID = m.Players_playerID and m.Team_teamID = t.teamid and t.teamid <> 0 and r.team_teamid = t.teamid
+        WHERE (name = '$name' OR mail = '$name') and password = '$mymd5' and p.playerID = m.Players_playerID and m.Team_teamID = t.teamid and t.teamid <> 0 and r.team_teamid = t.teamid) as x
+    WHERE (name = '$name' OR mail = '$name') and password = '$mymd5' and p.playerID = m.Players_playerID and m.Team_teamID = t.teamid and t.teamid <> 0 and r.team_teamid = t.teamid
     ORDER BY t.teamName;";		
 
 
