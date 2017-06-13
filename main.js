5492698314f802d7d8175e9f2b4332115ecc26bd
tniinisto@gmail.com <tniinsto@gmail.com>
2017-06-13 14:56:22 +0300

    geo testing continues...

--- main.js
+++ main.js
@@ -1157,7 +1157,7 @@ function initializeMap() {
 var nlat = 0, nlon = 0;
 
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(success, error, {maximumAge:Infinity, timeout:5000, nableHighAccuracy: true});
+        navigator.geolocation.getCurrentPosition(success, error, {maximumAge:5000, timeout:5000, enableHighAccuracy: true});
     } 
     else {
       //alert('geolocation not supported');
