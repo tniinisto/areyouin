bb18b9398db3b04de9fe3961a717600b4f5bb9e1
tniinisto@gmail.com <tniinsto@gmail.com>
2017-06-13 14:41:12 +0300

    geolocation tune

--- main.js
+++ main.js
@@ -1157,7 +1157,7 @@ function initializeMap() {
 var nlat = 0, nlon = 0;
 
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(success, error);
+        navigator.geolocation.getCurrentPosition(success, error, {timeout: 30000, enableHighAccuracy: true, maximumAge: 75000});
     } 
     else {
       //alert('geolocation not supported');
