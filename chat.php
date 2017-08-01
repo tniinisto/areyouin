1c927fbc4c06a74907bf6e4aad10606b9e01f5f7
tniinisto@gmail.com <tniinsto@gmail.com>
2017-08-01 16:36:38 +0300

    no affect

--- chat.php
+++ chat.php
@@ -23,11 +23,7 @@
 	        die('Could not connect: ' . mysql_error());
 	        }
 
-	    mysql_select_db($dbname, $con);
-
-mysql_query("SET NAMES 'utf8'");
-mysql_query("SET CHARACTER SET utf8");
-mysql_query("SET COLLATION_CONNECTION = 'utf8_unicode_ci'");        
+	    mysql_select_db($dbname, $con);     
 
         //Get current users info
         $sql5 = "SELECT name, photourl, pt.lastMsg as lastMsg FROM players, playerteam pt WHERE playerID = " . $playerid . " AND pt.Team_teamID = " . $teamid . " AND playerID = pt.Players_playerID";
