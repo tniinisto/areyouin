b7a94c630327740e9e4e941bd55736460a104575
tniinisto@gmail.com <tniinsto@gmail.com>
2017-08-04 15:11:46 +0300

    notify as str

--- updatePlayer.php
+++ updatePlayer.php
@@ -55,9 +55,10 @@
         // PDO. utf-8 //////////////////////////////////////////////////        
         $sql2 = "UPDATE players SET mail = :mail, mobile = :phone, notify = :notify, name = :name, firstname = :firstname, lastname = :lastname WHERE playerID = :playerid";
         $stmt2 = $dbh->prepare($sql2);
+
         $stmt2->bindParam(':mail', $player_email, PDO::PARAM_STR);
         $stmt2->bindParam(':phone', $player_phone, PDO::PARAM_STR);
-        $stmt2->bindParam(':notify', $player_notify PDO::PARAM_INT);
+        $stmt2->bindParam(':notify', $player_notify PDO::PARAM_STR);
         $stmt2->bindParam(':name', $player_name, PDO::PARAM_STR);
         $stmt2->bindParam(':firstname', $player_firstname, PDO::PARAM_STR);
         $stmt2->bindParam(':lastname', $player_lastname, PDO::PARAM_STR);
