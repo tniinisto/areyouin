d532215cdaf4dfa3cebf75bd2b125d7474f1daaf
tniinisto@gmail.com <tniinsto@gmail.com>
2017-08-04 14:42:43 +0300

    wonder why

--- updatePlayer.php
+++ updatePlayer.php
@@ -61,17 +61,16 @@
     //     // $result = mysql_query($sql);
 
     //     // // PDO. utf-8 //////////////////////////////////////////////////        
-    //     $sql2 ="UPDATE players SET mail = :mail, mobile = :phone, notify = :notify, name = :name, firstname = :firstname, lastname = :lastname WHERE playerID = :playerid";
-    //     $stmt2 = $dbh->prepare($sql2);
-    //     $stmt2->bindParam(':mail', $player_email, PDO::PARAM_STR);
-    //     $stmt2->bindParam(':phone', $player_phone, PDO::PARAM_STR);
-    //     $stmt2->bindParam(':notify', $player_notify PDO::PARAM_INT);
-    //     $stmt2->bindParam(':name', $player_name, PDO::PARAM_STR);
-    //     $stmt2->bindParam(':firstname', $player_firstname, PDO::PARAM_STR);
-    //     $stmt2->bindParam(':lastname', $player_lastname, PDO::PARAM_STR);
-    //     $stmt2->bindParam(':playerid', $playerid, PDO::PARAM_INT);
-
-    //     $result2 = $stmt2->execute();
+        $sql2 = "UPDATE players SET mail = :mail, mobile = :phone, notify = :notify, name = :name, firstname = :firstname, lastname = :lastname WHERE playerID = :playerid";
+        $stmt2 = $dbh->prepare($sql2);
+        $stmt2->bindParam(':mail', $player_email, PDO::PARAM_STR);
+        $stmt2->bindParam(':phone', $player_phone, PDO::PARAM_STR);
+        $stmt2->bindParam(':notify', $player_notify PDO::PARAM_INT);
+        $stmt2->bindParam(':name', $player_name, PDO::PARAM_STR);
+        $stmt2->bindParam(':firstname', $player_firstname, PDO::PARAM_STR);
+        $stmt2->bindParam(':lastname', $player_lastname, PDO::PARAM_STR);
+        $stmt2->bindParam(':playerid', $playerid, PDO::PARAM_INT);
+        $result2 = $stmt2->execute();
 
     //     if($_SESSION['ChromeLog']) { ChromePhp::log('Duplicate mail address, mysql_errno: ' . mysql_errno()); }
     
