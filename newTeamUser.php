a5e4185536162cd0aaf5d0b98aa7be4496a0c077
tniinisto@gmail.com <tniinsto@gmail.com>
2017-08-05 17:15:20 +0300

    utf-8 param to pdo

--- newTeamUser.php
+++ newTeamUser.php
@@ -11,7 +11,8 @@
     }
 
 
-  	$dbh = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);	
+  	//$dbh = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);
+    $dbh = new PDO("mysql:host=$dbhost;dbname=$dbname;charset=utf8", $dbuser, $dbpass);	//utf-8
 	$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         
     $password = '';
