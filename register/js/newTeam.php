cbf5a4059c8732cfb32fd533fe0e8c6012949a00
tniinisto <tniinisto@gmail.com>
2017-08-17 14:38:53 +0300

    new team chat comment

--- register/js/newTeam.php
+++ register/js/newTeam.php
@@ -130,7 +130,7 @@
         $stmt7 = $dbh->prepare($sql7);
 
         $ryouin_player = 0;
-        $comment = "Welcome to use R'YouIN! This is an automatic message from the team's registration.";
+        $comment = "Welcome to use R'YouIN! This is an automatic message from the team's registration. You can start by adding your teammates from the Admin - Users navigation and after that create a New event!";
         $stmt7->bindParam(':comment', $comment, PDO::PARAM_STR);
         $stmt7->bindParam(':playerid', $ryouin_player, PDO::PARAM_INT);
         $stmt7->bindParam(':teamid', $teamid_max, PDO::PARAM_INT);
