95e96a5e8f0a7d0b78f382bb4dfcfa6c1e4b9227
tniinisto@gmail.com <tniinsto@gmail.com>
2017-08-28 10:01:19 +0300

    admin modal helps fetching on document ready

--- main.js
+++ main.js
@@ -2209,31 +2209,32 @@ function startIPN() {
 /************************************************************************************
 Modal help dialog
 *************************************************************************************/
+function getAdminModalHelps() {
+    // Get the modal
+    var modalMail = document.getElementById('myModalMail');
 
-// Get the modal
-var modalMail = document.getElementById('myModalMail');
+    // Get the icon that opens the modal
+    var btn = document.getElementById("MailHelp");
 
-// Get the icon that opens the modal
-var btn = document.getElementById("MailHelp");
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("closeMail")[0];
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("closeMail")[0];
+    // When the user clicks the button, open the modal 
+    btn.onclick = function() {
+        modalMail.style.display = "block";
+    }
 
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-    modalMail.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modalMail.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modalMail) {
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
         modalMail.style.display = "none";
     }
-}
 
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modalMail) {
+            modalMail.style.display = "none";
+        }
+    }
+
+} 
 
