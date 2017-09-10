91930563ca4015c6c4a5da9b0e94260c90c739ea
tniinisto@gmail.com <tniinsto@gmail.com>
2017-09-10 20:14:32 +0300

    teamid parameter set to sql

--- PGmobile/services/events.php
+++ PGmobile/services/events.php
@@ -14,10 +14,10 @@ p.photourl, ep.EventPlayerID, ep.areyouin, ep.seen, t.teamID, t.teamName, pt.tea
 FROM
 (SELECT count(*) as rowcount
  FROM events ex, team t, eventplayer ep
- where ep.Events_eventID = ex.eventID and t.teamID = '3' and ex.endTime > now()) as x,
+ where ep.Events_eventID = ex.eventID and t.teamID = :teamid and ex.endTime > now()) as x,
 (SELECT count(*) as eventcount
  FROM events ex, team t
- where t.teamID = '3' and ex.endTime > now()) as y,
+ where t.teamID = :teamid and ex.endTime > now()) as y,
 events e
 inner join location l on l.locationID = e.Location_locationID
 inner join eventplayer ep on ep.Events_eventID = e.eventID
