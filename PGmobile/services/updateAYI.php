c98585a73d293a87ca722f9c5693ea2c4dd36618
tniinisto@gmail.com <tniinsto@gmail.com>
2017-10-02 18:14:31 +0300

    updateAYI service

---
+++ PGmobile/services/updateAYI.php
@@ -0,0 +1,33 @@
+<?php
+
+include( $_SERVER['DOCUMENT_ROOT'] . '/config/config.php' );
+
+
+$eventplayerid=$_POST['eventplayerid'];
+$ayi=$_POST['ayi'];
+
+
+try {
+    //PDO means "PHP Data Objects"
+    //dbh meand "Database handle"
+    //STH means "Statement Handle"
+
+	$dbh = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);	
+	$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $sql = "UPDATE eventplayer SET areyouin = :ayi WHERE EventPlayerID = :eventplayerid";		   
+    $stmt = $dbh->prepare($sql);
+    
+    $stmt->bindParam(':ayi', $ayi, PDO::PARAM_INT);
+    $stmt->bindParam(':eventplayerid', $eventplayerid, PDO::PARAM_INT);
+    $result = $stmt->execute();
+
+	$dbh = null;
+
+ 	//echo '{"items":'. json_encode($playerinfo) .'}'; 
+}
+catch(PDOException $e) {
+	echo '{"error":{"text":'. $e->getMessage() .'}}'; 
+}
+
+?>
