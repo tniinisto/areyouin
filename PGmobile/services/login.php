02d38092b48fb7afdf9c3f854cf1efd55e520218
tniinisto@gmail.com <tniinsto@gmail.com>
2017-10-04 19:05:04 +0300

    login sql tune

--- PGmobile/services/login.php
+++ PGmobile/services/login.php
@@ -28,7 +28,7 @@ $sql = "SELECT x.count, p.playerID, p.name, p.mail, p.photourl, t.teamID, t.team
         (SELECT count(*) as count
         FROM players p, playerteam m, team t, registration r
         WHERE (name = :name OR mail = :name) and password = :password and p.playerID = m.Players_playerID and m.Team_teamID = t.teamid and t.teamid <> 0 and r.team_teamid = t.teamid) as x
-    WHERE (name = '$name' OR mail = '$name') and password = :password and p.playerID = m.Players_playerID and m.Team_teamID = t.teamid and t.teamid <> 0 and r.team_teamid = t.teamid
+    WHERE (name = :name' OR mail = :name) and password = :password and p.playerID = m.Players_playerID and m.Team_teamID = t.teamid and t.teamid <> 0 and r.team_teamid = t.teamid
     ORDER BY t.teamName;";		
 
 
@@ -43,7 +43,8 @@ try {
     $stmt = $dbh->prepare($sql);
     $stmt->bindParam(':name',  $name, PDO::PARAM_STR);        
     $stmt->bindParam(':password',  $mymd5, PDO::PARAM_STR);        
-	$stmt = $dbh->query($sql);  
+    //$stmt = $dbh->query($sql);  
+    $stmt->execute();
 	$playerinfo = $stmt->fetchAll(PDO::FETCH_OBJ);
 
 	$dbh = null;
