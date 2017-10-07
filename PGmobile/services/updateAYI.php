372814f7b053dff6bf200748f2874f37902513c4
tniinisto@gmail.com <tniinsto@gmail.com>
2017-10-07 14:24:39 +0300

    update events last update time when ayi clicked

--- PGmobile/services/updateAYI.php
+++ PGmobile/services/updateAYI.php
@@ -6,6 +6,8 @@ include( $_SERVER['DOCUMENT_ROOT'] . '/config/config.php' );
 $eventplayerid=$_POST['eventplayerid'];
 $ayi=$_POST['ayi'];
 
+$playerid=$_POST['playerid'];
+$teamid=$_POST['teamid'];
 
 try {
     //PDO means "PHP Data Objects"
@@ -21,6 +23,17 @@ try {
     $stmt->bindParam(':ayi', $ayi, PDO::PARAM_INT);
     $stmt->bindParam(':eventplayerid', $eventplayerid, PDO::PARAM_INT);
     $result = $stmt->execute();
+
+
+//Update last event update time
+$updateDateTime = date("Y-n-j H:i:s");
+
+$sql2 = "UPDATE playerteam SET lastEventUpdate = :updatetime where Team_teamID = :teamid and players_playerId = :playerid ;";
+$stmt2 = $dbh->prepare($sql2);
+$stmt2->bindParam(':updatetime', $updateDateTime, PDO::PARAM_STR);
+$stmt2->bindParam(':teamid', $teamid, PDO::PARAM_INT);
+$stmt2->bindParam(':playerid', $playerid, PDO::PARAM_INT);
+$result2 = $stmt2->execute();
 
 	$dbh = null;
 
