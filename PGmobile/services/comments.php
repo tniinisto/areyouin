99873dc005f4084f52bb38653fb38597a8d3f4f8
tniinisto@gmail.com <tniinsto@gmail.com>
2017-10-13 13:33:09 +0300

    chat started

---
+++ PGmobile/services/comments.php
@@ -0,0 +1,38 @@
+<?php
+include( $_SERVER['DOCUMENT_ROOT'] . '/config/config.php' );
+
+//session_start();
+        
+// $pl=$_SESSION['myplayerid'];
+// $team=$_SESSION['myteamid'];
+
+$teamid=$_POST['teamid'];
+//$userid=$_POST['userid'];
+
+$sql = "SELECT c.*, p.photourl, p.name FROM comments c LEFT JOIN players p ON c.Players_playerID = p.playerID WHERE c.team_teamID = :teamid order by c.publishTime desc";
+
+try {
+    //PDO means "PHP Data Objects"
+    //dbh meand "Database handle"
+    //STH means "Statement Handle"
+
+    $dbh = new PDO("mysql:host=$dbhost;dbname=$dbname;charset=utf8", $dbuser, $dbpass);		
+	$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+    $stmt = $dbh->prepare($sql);
+    $stmt->bindParam(':teamid',  $teamid, PDO::PARAM_INT);        
+    $stmt->execute();
+    $event_info = $stmt->fetchAll(PDO::FETCH_OBJ);
+
+    $dbh = null;
+
+    echo '{"items":'. json_encode($comment_info) .'}'; 
+     
+}
+catch(PDOException $e) {
+    echo '{"error":{"text":'. $e->getMessage() .'}}'; 
+}
+
+
+
+?>
