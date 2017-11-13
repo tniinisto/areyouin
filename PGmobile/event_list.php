56d971b336580ca4e651a7003f94a67d922bb70a
tniinisto@gmail.com <tniinsto@gmail.com>
2017-11-13 13:03:40 +0200

    UTC test away

--- PGmobile/event_list.php
+++ PGmobile/event_list.php
@@ -34,7 +34,7 @@
 
         //Count timezone offset to support daylight savings
         $timezone=$_SESSION['mytimezone']; 
-        date_default_timezone_set( "UTC" );    
+
         $daylight_savings_offset_in_seconds = timezone_offset_get( timezone_open($timezone), new DateTime() ); 
         $team_offset = round($daylight_savings_offset_in_seconds/3600); //Hours        
                
