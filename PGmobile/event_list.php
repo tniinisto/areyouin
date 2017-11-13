8b8515736a3ffdbabe3514c3a0469bc539be4979
tniinisto@gmail.com <tniinsto@gmail.com>
2017-11-13 13:21:06 +0200

    offset test

--- PGmobile/event_list.php
+++ PGmobile/event_list.php
@@ -89,7 +89,7 @@
         inner join playerteam pt on pt.Players_playerID = p.playerID
         inner join team t on t.teamID = pt.Team_teamID
         where t.teamID = '" . $teamid  . "' and e.Team_teamID = t.teamID
-        and (e.endTime - INTERVAL " . $_SESSION['myoffset'] . " HOUR) > now()
+        and (e.endTime - INTERVAL " . $team_offset . " HOUR) > now()
         and ep.Events_eventID IN (". $eventIDs .")
         order by e.startTime asc, ep.Events_eventID asc, ep.areyouin desc, ep.seen desc;";
                     
