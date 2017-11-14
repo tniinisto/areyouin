1eb024c5e124f101ae4c6e9b9699de84939ff752
tniinisto@gmail.com <tniinsto@gmail.com>
2017-11-14 18:39:05 +0200

    date_default_timezone_set with quotations

--- event_list.php
+++ event_list.php
@@ -35,7 +35,7 @@
         //Count timezone offset to support daylight savings
         $timezone=$_SESSION['mytimezone']; 
 
-        date_default_timezone_set( $timezone );
+        date_default_timezone_set(" .  $timezone . ");
 
         //Local date
         $now = new DateTime(null, new DateTimeZone($timezone));    
@@ -66,7 +66,7 @@
 
         //Get events in set limit
         $sql_events = "SELECT SQL_CALC_FOUND_ROWS e.eventID, e.startTime FROM events e
-                       where e.Team_teamID = '" . $teamid  . "' and DATETIME(e.endTime) > " . $now->format('Y-m-d H:i:s') . "
+                       where e.Team_teamID = '" . $teamid  . "' and e.endTime > now()
                        order by e.startTIme asc
                        LIMIT " . MAX_NRO_EVENTS . " OFFSET " . $offset . ";";
         $rows_events = mysql_query($sql_events);        
@@ -95,7 +95,7 @@
         inner join playerteam pt on pt.Players_playerID = p.playerID
         inner join team t on t.teamID = pt.Team_teamID
         where t.teamID = '" . $teamid  . "' and e.Team_teamID = t.teamID
-        and DATETIME(e.endTime) > " . $now->format('Y-m-d H:i:s') . "
+        and e.endTime > now()
         and ep.Events_eventID IN (". $eventIDs .")
         order by e.startTime asc, ep.Events_eventID asc, ep.areyouin desc, ep.seen desc;";
                     
